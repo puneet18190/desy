d96036af10911d09d5001c49a4d79ebc54354fce
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-09-24 12:36:25 +0200

    commit iniziale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Desy::Application.config.secret_token = '9694a561307b34bb27cd4d965d1213e47ef085c2962550e9a9c61b1118ff60387b1f259d66b915bb7bbfa7708c3724c05d412fbc8e50e05b274d44e59b26a2bf'
