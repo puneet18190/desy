c7560559ce5a8296781ec8fdbc20bd57863750f8
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-09-24 17:21:55 +0200

    associazioni interne ai modelli, aggiunte cascate per polimorfiche

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,9 +3,9 @@ class Lesson < ActiveRecord::Base
   belongs_to :user
   belongs_to :subject
   belongs_to :school_level
-  has_many :bookmarks, :as => :bookmarkable
-  has_many :likes, :as => :likeable
-  has_many :reports, :as => :reportable
+  has_many :bookmarks, :as => :bookmarkable, :dependent => :destroy
+  has_many :likes, :as => :likeable, :dependent => :destroy
+  has_many :reports, :as => :reportable, :dependent => :destroy
   has_many :slides
   has_many :virtual_classroom_lessons
   
--- app/models/media_element.rb
+++ app/models/media_element.rb
@@ -1,9 +1,9 @@
 class MediaElement < ActiveRecord::Base
   
-  has_many :bookmarks, :as => :bookmarkable
-  has_many :likes, :as => :likeable
+  has_many :bookmarks, :as => :bookmarkable, :dependent => :destroy
+  has_many :likes, :as => :likeable, :dependent => :destroy
   has_many :media_elements_slides
-  has_many :reports, :as => :reportable
+  has_many :reports, :as => :reportable, :dependent => :destroy
   belongs_to :user
   
 end
