5e9edd8a02fe52ca73583d8825cfe88b3ca65c02
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-09-28 09:26:11 +0200

    location validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,10 @@
 class Location < ActiveRecord::Base
   
+  attr_accessible :description
+  
   has_many :users
+  
+  validates_presence_of :description
+  validates_length_of :description, :maximum => 255
   
 end
--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    begin
+      @location = Location.new :description => 'Scuola Primaria'
+    rescue ActiveModel::MassAssignmentSecurity::Error
+      @location = nil
+    end
+  end
+  
+  test 'null' do
+    assert !@location.nil?
+    @location = Location.new
+    assert_error_size 1, @location
+  end
+  
+  test 'types' do
+    assert_invalid @location, :description, long_string(256), long_string(255), /is too long/
+    assert_obj_saved @location
+  end
+  
 end
