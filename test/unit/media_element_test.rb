c21a9f47679fafc7790e206baff00efa32ee5e1e
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-09-28 14:28:12 +0200

    sf

--- test/unit/media_element_test.rb
+++ test/unit/media_element_test.rb
@@ -4,7 +4,10 @@ class MediaElementTest < ActiveSupport::TestCase
   
   def setup
     begin
-      @media_element = MediaElement.new :description => 'Scuola Primaria'
+      @media_element = MediaElement.new :description => 'Scuola Primaria', :title => 'Scuola'
+      @media_element.user_id = 1
+      @media_element.sti_type = 'Video'
+      @media_element.duration = 10
     rescue ActiveModel::MassAssignmentSecurity::Error
       @media_element = nil
     end
@@ -24,10 +27,18 @@ class MediaElementTest < ActiveSupport::TestCase
     assert_raise(ActiveModel::MassAssignmentSecurity::Error) {MediaElement.new(:user_id => 1)}
   end
   
- # test 'types' do
- #   assert_invalid @media_element, :description, long_string(256), long_string(255), /is too long/
- #   assert_obj_saved @media_element
- # end
+  test 'types' do
+    assert_invalid @media_element, :title, long_string(256), long_string(255), /is too long/
+    assert_invalid @media_element, :user_id, 'po', 1, /is not a number/
+    assert_invalid @media_element, :user_id, -3, 1, /must be greater than 0/
+    assert_invalid @media_element, :user_id, 3.4, 1, /must be an integer/
+    assert_invalid @media_element, :duration, 'po', 1, /is not a number/
+    assert_invalid @media_element, :duration, -3, 1, /must be greater than 0/
+    assert_invalid @media_element, :duration, 3.4, 1, /must be an integer/
+    assert_invalid @media_element, :is_public, nil, false, /is not included in the list/
+    assert_invalid @media_element, :sti_type, 'Film', 'Video', /is not included in the list/
+    assert_obj_saved @media_element
+  end
   
   test 'association_methods' do
     assert_nothing_raised {@media_element.bookmarks}
