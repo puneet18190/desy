6fdfb563fc4bfafeaa3652f6172210f90f700321
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-09-28 09:37:20 +0200

    subject test

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,6 +1,11 @@
 class Subject < ActiveRecord::Base
   
+  attr_accessible :description
+  
   has_many :lessons
   has_many :users_subjects
+  
+  validates_presence_of :description
+  validates_length_of :description, :maximum => 255
   
 end
--- test/unit/subject_test.rb
+++ test/unit/subject_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class SubjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    begin
+      @subject = Subject.new :description => 'Scuola Primaria'
+    rescue ActiveModel::MassAssignmentSecurity::Error
+      @subject = nil
+    end
+  end
+  
+  test 'null' do
+    assert !@subject.nil?
+    @subject = Subject.new
+    assert_error_size 1, @subject
+  end
+  
+  test 'types' do
+    assert_invalid @subject, :description, long_string(256), long_string(255), /is too long/
+    assert_obj_saved @subject
+  end
+  
 end
