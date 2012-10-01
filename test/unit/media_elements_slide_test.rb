3d503da29a82ddd004c031d307408ec7264a2e9f
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-01 12:36:14 +0200

    test media_elements_slide

--- test/unit/media_elements_slide_test.rb
+++ test/unit/media_elements_slide_test.rb
@@ -2,31 +2,38 @@ require 'test_helper'
 
 class MediaElementsSlideTest < ActiveSupport::TestCase
   
- # def setup
- #   begin
- #     @media_elements_slide = MediaElementsSlide.new :description => 'Scuola Primaria'
- #   rescue ActiveModel::MassAssignmentSecurity::Error
- #     @media_elements_slide = nil
- #   end
- # end
+  def setup
+    @new_slide = Slide.new :position => 2, :title => 'Titolo', :text1 => 'Testo testo testo'
+    @new_slide.lesson_id = 1
+    @new_slide.kind = 'video1'
+    begin
+      @media_elements_slide = MediaElementsSlide.new :position => 1
+      @media_elements_slide.slide_id = @new_slide.id
+      @media_elements_slide.media_element_id = 2
+    rescue ActiveModel::MassAssignmentSecurity::Error
+      @media_elements_slide = nil
+    end
+  end
   
- # test 'empty_and_defaults' do
- #   @media_elements_slide = MediaElementsSlide.new
- #   assert_error_size 1, @media_elements_slide
- # end
+  #test 'empty_and_defaults' do
+  #  @media_elements_slide = MediaElementsSlide.new
+  #  assert_error_size 1, @media_elements_slide
+  #end
   
- # test 'attr_accessible' do
- #   assert !@media_elements_slide.nil?
- # end
+  test 'attr_accessible' do
+    assert !@media_elements_slide.nil?
+    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {MediaElementsSlide.new(:slide_id => 1)}
+    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {MediaElementsSlide.new(:media_element_id => 1)}
+  end
   
  # test 'types' do
  #   assert_invalid @media_elements_slide, :description, long_string(256), long_string(255), /is too long/
- #   assert_obj_saved @media_elements_slide
- # end
+  #  assert_obj_saved @media_elements_slide
+  #end
   
- # test 'association_methods' do
- #   assert_nothing_raised {@media_elements_slide.users}
- #   assert_nothing_raised {@media_elements_slide.lessons}
- # end
+  test 'association_methods' do
+    assert_nothing_raised {@media_elements_slide.media_element}
+    assert_nothing_raised {@media_elements_slide.slide}
+  end
   
 end
