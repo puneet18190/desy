fa711e76adbea5847543bc6356092f76c3207fdb
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-01 11:25:57 +0200

    test unicità position slide

--- test/unit/slide_test.rb
+++ test/unit/slide_test.rb
@@ -38,6 +38,21 @@ class SlideTest < ActiveSupport::TestCase
     assert_nothing_raised {@slide.lesson}
   end
   
+  test 'uniqueness' do
+    @slide.lesson_id = 2
+    assert_invalid @slide, :position, 2, 4, /has already been taken/
+    # I rewrite manually assert_invalid
+    @slide.kind = 'cover'
+    assert !@slide.save, "Slide erroneously saved - #{@slide.inspect}"
+    assert_equal 2, @slide.errors.messages.length, "A field which wasn't supposed to be affected returned error - #{@slide.errors.inspect}"
+    assert_match /has already been taken/, @slide.errors.messages[:kind].first
+    @slide.kind = 'image1'
+    assert @slide.valid?, "Slide not valid: #{@slide.errors.inspect}"
+    # until here
+    assert_obj_saved @slide
+    assert_equal 2, Slide.where(:lesson_id => 2, :kind => 'image1').count
+  end
+  
 # nel test della callback before destroy per la copertina, anche metti un ok_destruction test che mi permette di verificare che posso cancellare la slide e nient'altro! ++ metti nei fixtures una copertina per ogni lezione già caricata!
   
 end
