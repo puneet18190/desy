ba04430cc5f2e3bf432c8c6c4e159784d0d009e6
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-01 15:11:31 +0200

    aggiunto e testato validatore

---
+++ lib/autoload/valid.rb
@@ -0,0 +1,35 @@
+module Valid
+  
+  class Validness
+    
+    def get object, column
+      column = column.to_s
+      if column == 'id'
+        return object.new_record? ? nil : object.class.where(:id => object.id).first
+      else
+        my_class = get_class_from_column_name column
+        original_column = object.read_attribute_before_type_cast(column)
+        return original_column.kind_of?(Integer) ? my_class.where(:id => original_column).first : nil
+      end
+    end
+    
+    private
+    
+    def get_class_from_column_name x
+      resp = ''
+      x.split('_').each do |my_split|
+        if my_split != 'id'
+          resp = "#{resp}#{my_split.capitalize}"
+        end
+      end
+      return resp.constantize
+    end
+    
+  end
+  
+  def self.get_association object, column
+    x = Validness.new
+    x.get object, column
+  end
+  
+end
