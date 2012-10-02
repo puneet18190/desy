baa3084f6676dafa4029c66bd09808f68ddd7219
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-02 11:36:38 +0200

    tag and test

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,27 @@
 class Tag < ActiveRecord::Base
   
+  MIN_LENGTH = VARIABLES['min_tag_length']
+  MAX_LENGTH = (VARIABLES['max_tag_length'] > 255 ? 255 : VARIABLES['max_tag_length'])
+  
+  attr_accessible :word
+  
   has_many :taggings
+  
+  validates_presence_of :word
+  validates_length_of :word, :minimum => MIN_LENGTH, :maximum => MAX_LENGTH
+  validates_uniqueness_of :word
+  
+  before_validation :convert_downcase_word
+  
+  private
+  
+  def convert_downcase_word
+    return if self.word.blank?
+    self.word = downcase_international_tag self.word
+  end
+  
+  def downcase_international_tag x
+    x.downcase # qui sostituirò il metodo a seconda del linguaggio, da configurare!
+  end
   
 end
--- test/unit/tag_test.rb
+++ test/unit/tag_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    begin
+      @tag = Tag.new :word => 'passerotto'
+    rescue ActiveModel::MassAssignmentSecurity::Error
+      @tag = nil
+    end
+  end
+  
+  test 'empty_and_defaults' do
+    @tag = Tag.new
+    assert_error_size 2, @tag
+  end
+  
+  test 'attr_accessible' do
+    assert !@tag.nil?
+  end
+  
+  test 'types' do
+    assert_invalid @tag, :word, long_string(21), long_string(20), /is too long/
+    assert_invalid @tag, :word, 'er', 'oca', /is too short/
+    assert_obj_saved @tag
+  end
+  
+  test 'uniqueness' do
+    assert_invalid @tag, :word, 'squalo', 'passerotto', /has already been taken/
+    assert_obj_saved @tag
+  end
+  
+  test 'association_methods' do
+    assert_nothing_raised {@tag.taggings}
+  end
+  
 end
