732718933d24041ddb97ba8351dbbc9b9bbe307b
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-02 12:18:56 +0200

    GA2

--- test/unit/tagging_test.rb
+++ test/unit/tagging_test.rb
@@ -15,9 +15,9 @@ class TaggingTest < ActiveSupport::TestCase
   end
   
   test 'attr_accessible' do
-    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {Bookmark.new(:tag_id => 1)}
-    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {Bookmark.new(:taggable_id => 2)}
-    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {Bookmark.new(:taggable_type => 'MediaElement')}
+    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {Tagging.new(:tag_id => 1)}
+    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {Tagging.new(:taggable_id => 2)}
+    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {Tagging.new(:taggable_type => 'MediaElement')}
   end
   
   test 'types' do
@@ -31,6 +31,23 @@ class TaggingTest < ActiveSupport::TestCase
   test 'association_methods' do
     assert_nothing_raised {@tagging.tag}
     assert_nothing_raised {@tagging.taggable}
+  end
+  
+  test 'uniqueness' do
+    @tagging.tag_id = 3
+    assert_invalid @tagging, :taggable_id, 1, 2, /has already been taken/
+    @tagging.taggable_type = 'Lesson'
+    @tagging.tag_id = 2
+    assert_invalid @tagging, :taggable_id, 1, 2, /has already been taken/
+    assert_obj_saved @tagging
+  end
+  
+  test 'associations' do
+    assert_invalid @tagging, :tag_id, 1000, 1, /doesn't exist/
+    assert_invalid @tagging, :taggable_id, 1000, 2, /media element doesn't exist/
+    @tagging.taggable_type = 'Lesson'
+    assert_invalid @tagging, :taggable_id, 1000, 1, /lesson doesn't exist/
+    assert_obj_saved @tagging
   end
   
 end
