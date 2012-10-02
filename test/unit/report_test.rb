11e06f68d254c648a0d160d0e2aa82b149965d62
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-02 14:18:37 +0200

    iniziato test report

--- test/unit/report_test.rb
+++ test/unit/report_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @report = Report.new
+    @report.user_id = 1
+    @report.reportable_type = 'Lesson'
+    @report.reportable_id = 1
+    @report.comment = "Non sono d'accordo con il contenuto"
+  end
+  
+  test 'empty_and_defaults' do
+    @report = Report.new
+    assert_error_size 7, @report
+  end
+  
+  test 'attr_accessible' do
+    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {Report.new(:user_id => 1)}
+    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {Report.new(:reportable_id => 2)}
+    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {Report.new(:reportable_type => 'MediaElement')}
+    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {Report.new(:comment => 'Fa schifo!')}
+  end
+  
+  #test 'types' do
+  #  assert_invalid @report, :description, long_string(256), long_string(255), /is too long/
+  ##  assert_obj_saved @report
+  #end
+  
+  test 'association_methods' do
+    assert_nothing_raised {@report.user}
+    assert_nothing_raised {@report.reportable}
+  end
+  
 end
