75decdf0ec669c501049eaba554a53863753d803
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-02 17:17:59 +0200

    finito test likes

--- test/unit/like_test.rb
+++ test/unit/like_test.rb
@@ -30,6 +30,41 @@ class LikeTest < ActiveSupport::TestCase
     assert_nothing_raised {@like.lesson}
   end
   
+  test 'uniqueness' do
+    @like.user_id = 1
+    @like.lesson_id = 2
+    assert !@like.save, "Like erroneously saved - #{@like.inspect}"
+    assert_equal 1, @like.errors.messages.length, "A field which wasn't supposed to be affected returned error - #{@like.errors.inspect}"
+    assert_equal 1, @like.errors.size
+    assert_match /has already been taken/, @like.errors.messages[:lesson_id].first
+    @like.user_id = 2
+    @like.lesson_id = 1
+    assert_obj_saved @like
+  end
   
+  test 'associations' do
+    assert_invalid @like, :user_id, 1000, 2, /doesn't exist/
+    assert_invalid @like, :lesson_id, 1000, 1, /doesn't exist/
+    assert_obj_saved @like
+  end
+  
+  test 'validity' do
+    assert_invalid @like, :lesson_id, 2, 1, /can't be liked/
+    assert_obj_saved @like
+  end
+  
+  test 'impossible_changes' do
+    assert_obj_saved @like
+    @lesson = Lesson.new :subject_id => 1, :school_level_id => 2, :title => 'Fernandello mio', :description => 'Voglio divenire uno scienziaaato'
+    @lesson.copied_not_modified = false
+    @lesson.user_id = 1
+    @lesson.save
+    @user = User.new :name => 'Javier Ernesto', :surname => 'Chevanton', :school_level_id => 1,
+      :school => 'Scuola', :location_id => 1
+    @user.email = 'plutdsgso@pippo.it'
+    assert_invalid @like, :user_id, @user.id, 2, /can't be changed/
+    assert_invalid @like, :lesson_id, @lesson.id, 1, /can't be changed/
+    assert_obj_saved @like
+  end
   
 end
