e0ff546415e0bf4982768d35971869e3c583f342
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-02 12:08:58 +0200

    sd

--- test/unit/tagging_test.rb
+++ test/unit/tagging_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class TaggingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @tagging = Tagging.new
+    @tagging.tag_id = 1
+    @tagging.taggable_id = 2
+    @tagging.taggable_type = 'MediaElement'
+  end
+  
+  test 'empty_and_defaults' do
+    @tagging = Tagging.new
+    assert_error_size 6, @tagging
+  end
+  
+  test 'attr_accessible' do
+    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {Bookmark.new(:tag_id => 1)}
+    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {Bookmark.new(:taggable_id => 2)}
+    assert_raise(ActiveModel::MassAssignmentSecurity::Error) {Bookmark.new(:taggable_type => 'MediaElement')}
+  end
+  
+  test 'types' do
+    assert_invalid @tagging, :tag_id, 'rt', 1, /is not a number/
+    assert_invalid @tagging, :tag_id, 9.9, 1, /must be an integer/
+    assert_invalid @tagging, :taggable_id, -8, 2, /must be greater than 0/
+    assert_invalid @tagging, :taggable_type, 'MidiaElement', 'MediaElement', /is not included in the list/
+    assert_obj_saved @tagging
+  end
+  
+  test 'association_methods' do
+    assert_nothing_raised {@tagging.tag}
+    assert_nothing_raised {@tagging.taggable}
+  end
+  
 end
