0f9322dc2cd7b8df22dfb18a2b3b093251f48299
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-03 11:44:24 +0200

    aggiunta associazione parent e copies a lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,6 +5,8 @@ class Lesson < ActiveRecord::Base
   belongs_to :user
   belongs_to :subject
   belongs_to :school_level
+  belongs_to :parent, :class_name => 'Lesson', :foreign_key => :parent_id
+  has_many :copies, :class_name => 'Lesson', :foreign_key => :parent_id
   has_many :bookmarks, :as => :bookmarkable, :dependent => :destroy
   has_many :likes
   has_many :reports, :as => :reportable, :dependent => :destroy
