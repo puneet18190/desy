09cb1b1d47abf78814e40e787b282a1ccf5f2195
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-09 16:49:25 +0200

    introdotta paginazione dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,16 @@
 class DashboardController < ApplicationController
   
+  LESSONS_FOR_PAGE = CONFIG['lessons_for_page_in_dashboard']
+  LESSON_PAGES = CONFIG['lesson_pages_in_dashboard']
+  ELEMENTS_FOR_PAGE = CONFIG['elements_for_page_in_dashboard']
+  ELEMENT_PAGES = CONFIG['element_pages_in_dashboard']
+  
   def index
-    @lessons = Lesson.all
-    @media_elements = MediaElement.all
-    @notifications = Notification.where(:user_id => @current_user.id)
+    @lessons = @current_user.suggested_lessons(LESSONS_FOR_PAGE * LESSON_PAGES)
+    @media_elements = @current_user.suggested_elements(ELEMENTS_FOR_PAGE * ELEMENT_PAGES)
+    @notifications = Notification.where(:user_id => @current_user.id).order('created_at DESC')
+    @alert_notifications = Notification.where(:user_id => @current_user.id, :seen => false).any?
+    @where = 'dashboard'
   end
   
 end
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,6 +2,7 @@ class LessonsController < ApplicationController
   
   def index
     @lessons = @current_user.lessons
+    @where = 'lessons'
   end
   
 end
