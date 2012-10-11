1ab422759077eb4381b9af3043080989c91c45e4
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-11 15:44:48 +0200

    ageg

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,7 +1,7 @@
 function switchToSuggestedMediaElements() {
   $('.suggested_lessons_page').css('display', 'none');
   $('.suggested_media_elements_page').css('display', 'none');
-  $('#suggested_media_elements_0').css('display', 'block');
+  $('#suggested_media_elements_1').css('display', 'block');
   $('#switch_to_media_elements').addClass('current');
   $('#switch_to_lessons').removeClass('current');
 }
@@ -9,7 +9,7 @@ function switchToSuggestedMediaElements() {
 function switchToSuggestedLessons() {
   $('.suggested_lessons_page').css('display', 'none');
   $('.suggested_media_elements_page').css('display', 'none');
-  $('#suggested_lessons_0').css('display', 'block');
+  $('#suggested_lessons_1').css('display', 'block');
   $('#switch_to_lessons').addClass('current');
   $('#switch_to_media_elements').removeClass('current');
 }
