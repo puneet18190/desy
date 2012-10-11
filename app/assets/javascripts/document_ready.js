ab598d159162d29410014fc9297de075bf2190d6
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-11 11:32:43 +0200

    creato document_ready.js

---
+++ app/assets/javascripts/document_ready.js
@@ -0,0 +1,16 @@
+$(document).ready(function {
+  
+  $('#switch_to_lessons').click(function() {
+    switchToSuggestedLessons();
+  });
+  
+  $('#switch_to_media_elements').click(function() {
+    switchToSuggestedMediaElements();
+  });
+  
+  $('#filter_lessons').change(function() {
+    var filter = $'#filter_lessons').val();
+    window.location.href = '/lessons?filter=' + filter
+  });
+  
+});
