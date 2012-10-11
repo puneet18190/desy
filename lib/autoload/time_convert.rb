c5a631a25ef8fc61184bb11500fce8c60c269d92
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-11 16:38:17 +0200

    time converter

--- lib/autoload/time_convert.rb
+++ lib/autoload/time_convert.rb
@@ -4,19 +4,76 @@ module TimeConvert
     
     def to_string a_time
       return '' if !a_time.kind_of?(Time)
-      return 'blalblabl'
+      case I18n.t('time.format')
+        when 'english'
+          return convert_in_english a_time
+        when 'chinese'
+          return convert_in_chinese a_time
+        when 'italian'
+          return convert_in_italian a_time
+        else
+          return a_time.to_s
+      end
     end
     
     private
     
-    def get_class_from_column_name x
-      resp = ''
-      x.split('_').each do |my_split|
-        if my_split != 'id'
-          resp = "#{resp}#{my_split.capitalize}"
-        end
+    def convert_in_english a_time
+      day = a_time.day
+      month = a_time.month
+      year = a_time.year
+      return "#{english_month(month)} #{day}#{english_day_suffix(day)}, #{year}"
+    end
+    
+    def english_day_suffix x
+      if [1, 21, 31].include? x
+        return 'st'
+      elsif [2, 22].include? x
+        return 'nd'
+      elsif [3, 23].include? x
+        return 'rd'
+      else
+        return 'th'
       end
-      return resp.constantize
+    end
+    
+    def english_month x
+      case x
+        when 1
+          return 'January'
+        when 2
+          return 'February'
+        when 3
+          return 'March'
+        when 4
+          return 'April'
+        when 5
+          return 'May'
+        when 6
+          return 'June'
+        when 7
+          return 'July'
+        when 8
+          return 'August'
+        when 9
+          return 'September'
+        when 10
+          return 'October'
+        when 11
+          return 'November'
+        when 12
+          return 'December'
+        else
+          return ''
+      end
+    end
+    
+    def convert_in_chinese a_time
+      a_time.to_s
+    end
+    
+    def conver_in_italian a_time
+      a_time.to_s
     end
     
   end
