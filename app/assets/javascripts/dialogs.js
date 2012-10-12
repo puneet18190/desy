30fa2a6b1430fc622287d546c7d1f2d70c3c8eab
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-12 17:34:36 +0200

    funzioni standard per aprire i pop ups

--- app/assets/javascripts/dialogs.js
+++ app/assets/javascripts/dialogs.js
@@ -1,11 +1,17 @@
-function showNormalPopUp() {
+function showNormalPopUp(content, title) {
+  $("#dialog-normal").attr('title', title);
+  $("#dialog-normal").html(content);
   $("#dialog-normal").dialog("open");
 }
 
-function showShadePopUp() {
+function showShadePopUp(content, title) {
+  $("#dialog-shade").attr('title', title);
+  $("#dialog-shade").html(content);
   $("#dialog-shade").dialog("open");
 }
 
-function showTimedPopUp() {
+function showTimedPopUp(content, title) {
+  $("#dialog-timed").attr('title', title);
+  $("#dialog-timed").html(content);
   $("#dialog-timed").dialog("open");
 }
