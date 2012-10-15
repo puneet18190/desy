36ffc6d1aa2662dae5d57fa81805a6eb775af06f
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-15 15:45:19 +0200

    viste js.erb

--- app/controllers/virtual_classroom_controller.rb
+++ app/controllers/virtual_classroom_controller.rb
@@ -1,2 +1,29 @@
 class VirtualClassroomController < ApplicationController
+  
+  def add_lesson
+    initialize_lesson
+    if @ok
+      if !@lesson.add_to_virtual_classroom(@current_user.id)
+        @ok = false
+        @error = @lesson.get_base_error
+      end
+    else
+      @error = I18n.t('activerecord.errors.models.lesson.problem_adding_to_virtual_classroom')
+    end
+    reload_lesson
+  end
+  
+  def remove_lesson # qui ci vuole un filtro
+    initialize_lesson
+    if @ok
+      if !@lesson.remove_from_virtual_classroom(@current_user.id)
+        @ok = false
+        @error = @lesson.get_base_error
+      end
+    else
+      @error = I18n.t('activerecord.errors.models.lesson.problem_removing_from_virtual_classroom')
+    end
+    reload_lesson
+  end
+  
 end
