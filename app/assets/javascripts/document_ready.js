ef16a72ea3fe6a7f4a686726eefa5b8204f709ca
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-16 17:34:16 +0200

    buttons

--- app/assets/javascripts/document_ready.js
+++ app/assets/javascripts/document_ready.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   
+  
+  // DASHBOARD SWITCH
+  
   $('#switch_to_lessons').click(function() {
     switchToSuggestedLessons();
   });
@@ -8,10 +11,16 @@ $(document).ready(function() {
     switchToSuggestedMediaElements();
   });
   
+  
+  // FILTERS
+  
   $('#filter_lessons').change(function() {
     var filter = $('#filter_lessons option:selected').val();
     window.location.href = '/lessons?filter=' + filter;
   });
+  
+  
+  // EXPAND LESSON
   
   $('body').on('click','._lesson_compact',function() {
     var my_id = this.id;
@@ -22,6 +31,9 @@ $(document).ready(function() {
       my_expanded.css('display', 'block');
     }
   });
+  
+  
+  // DIALOGS AND POPUPS
   
   $("#dialog-error").dialog({
     autoOpen: false,
@@ -35,13 +47,119 @@ $(document).ready(function() {
     autoOpen: false,
   });
   
-  $('body').on('click', '._action_button.publish, ._action_button.unpublish', function(e) {
+  
+  // LESSON BUTTONS
+  
+  $('body').on('click', '._Lesson_button_add', function(e) {
     e.preventDefault();
-    var my_function = $(this).data('clickfunction');
     var my_param = $(this).data('clickparam');
-    window[my_function](my_param);
+    addLesson(my_param);
     return false;
   });
+  
+  $('body').on('click', '._Lesson_button_copy', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    copyLesson(my_param);
+    return false;
+  });
+  
+  $('body').on('click', '._Lesson_button_destroy', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    destroyLesson(my_param);
+    return false;
+  });
+  
+  $('body').on('click', '._Lesson_button_dislike', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    dislikeLesson(my_param);
+    return false;
+  });
+  
+  $('body').on('click', '._Lesson_button_like', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    likeLesson(my_param);
+    return false;
+  });
+  
+  $('body').on('click', '._Lesson_button_preview', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    previewLesson(my_param);
+    return false;
+  });
+  
+  $('body').on('click', '._Lesson_button_publish', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    publishLesson(my_param);
+    return false;
+  });
+  
+  $('body').on('click', '._Lesson_button_remove', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    removeLesson(my_param);
+    return false;
+  });
+  
+  $('body').on('click', '._Lesson_button_unpublish', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    unpublishLesson(my_param);
+    return false;
+  });
+  
+  $('body').on('click', '._Lesson_button_add_virtual_classroom', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    addLessonToVirtualClassroom(my_param);
+    return false;
+  });
+  
+  $('body').on('click', '._Lesson_button_remove_virtual_classroom', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    removeLessonFromVirtualClassroom(my_param);
+    return false;
+  });
+  
+  
+  // MEDIA ELEMENT BUTTONS
+  
+  $('body').on('click', '._MediaElement_button_add', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    addMediaElement(my_param);
+    return false;
+  });
+  
+  $('body').on('click', '._MediaElement_button_destroy', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    destroyMediaElement(my_param);
+    return false;
+  });
+  
+  $('body').on('click', '._MediaElement_button_preview', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    previewMediaElement(my_param);
+    return false;
+  });
+  
+  $('body').on('click', '._MediaElement_button_remove', function(e) {
+    e.preventDefault();
+    var my_param = $(this).data('clickparam');
+    removeMediaElement(my_param);
+    return false;
+  });
+  
+  
+  // JAVASCRIPT ANIMATIONS
   
   $('.scroll-pane').jScrollPane({
     autoReinitialise: true
@@ -51,10 +169,13 @@ $(document).ready(function() {
   
   $("#filter_lessons").selectbox();
   
-	$("#format_media_elements").selectbox();
-	
-	$("#filter_media_elements").selectbox();
-
+  $("#format_media_elements").selectbox();
+  
+  $("#filter_media_elements").selectbox();
+  
+  
+  // NOTIFICATIONS
+  
   initializeNotifications();
   
 });
