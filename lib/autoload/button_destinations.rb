d1466348dd33436408ee9ded6fd1a9307d4e2ed7
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-17 12:04:24 +0200

    button_destination

---
+++ lib/autoload/button_destinations.rb
@@ -0,0 +1,12 @@
+module ButtonDestinations
+  
+  EXPANDED_LESSON = 'expanded_lesson'
+  COMPACT_LESSON = 'compact_lesson'
+  EXPANDED_MEDIA_ELEMENT = 'expanded_media_element'
+  COMPACT_MEDIA_ELEMENT = 'compact_media_element'
+  POPUP_MEDIA_ELEMENT = 'popup_media_element'
+  
+  LESSONS = [EXPANDED_LESSON, COMPACT_LESSON]
+  MEDIA_ELEMENTS = [EXPANDED_MEDIA_ELEMENT, COMPACT_MEDIA_ELEMENT, POPUP_MEDIA_ELEMENT]
+  
+end
