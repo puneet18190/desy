5fb30eee74c73b8dc1fe1e1b8054b9933531da29
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-18 14:15:57 +0200

    daggsd

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,11 @@
 class SearchController < ApplicationController
+  
+  def lessons
+    @lessons = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+  
+  def media_elements
+    @media_elements = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+  
 end
