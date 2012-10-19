a0964c598e572228a87b0bf4ba25fc76fd36e4bf
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-19 15:21:34 +0200

    modificata vista

--- app/models/my_lessons_view.rb
+++ app/models/my_lessons_view.rb
@@ -1,2 +1,5 @@
 class MyLessonsView < ActiveRecord::Base
+  
+  set_table_name 'my_lessons_view'
+  
 end
--- app/models/my_media_elements_view.rb
+++ app/models/my_media_elements_view.rb
@@ -1,2 +1,5 @@
 class MyMediaElementsView < ActiveRecord::Base
+  
+  set_table_name 'my_media_elements_view'
+  
 end
