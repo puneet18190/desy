6bf2f34d51881e67dc266b2482eb6818ddc0f4b7
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-10-24 14:58:52 +0200

    rimosso publication date order

--- lib/autoload/search_orders.rb
+++ lib/autoload/search_orders.rb
@@ -3,16 +3,14 @@ module SearchOrders
   UPDATED_AT = 'updated_at'
   LIKES = 'likes'
   TITLE = 'title'
-  PUBLICATION_DATE = 'publication_date'
   
   LESSONS_SET = [UPDATED_AT, LIKES, TITLE]
-  MEDIA_ELEMENTS_SET = [UPDATED_AT, PUBLICATION_DATE, TITLE]
+  MEDIA_ELEMENTS_SET = [UPDATED_AT, TITLE]
   
   CAPTIONS = {
     UPDATED_AT => I18n.t('orders.updated_at'),
     LIKES => I18n.t('orders.likes'),
-    TITLE => I18n.t('orders.title'),
-    PUBLICATION_DATE => I18n.t('orders.publication_date')
+    TITLE => I18n.t('orders.title')
   }
   
 end
