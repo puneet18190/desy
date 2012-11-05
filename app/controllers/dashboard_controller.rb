e39344783803a997dd0d24bb3ae8587694ce2ece
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-11-05 10:04:48 +0100

    paginazione dashboard quando svuoto l'ultima pagina

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,18 +1,22 @@
 class DashboardController < ApplicationController
   
-  LESSONS_FOR_PAGE = CONFIG['lessons_for_page_in_dashboard']
-  LESSON_PAGES = CONFIG['lesson_pages_in_dashboard']
-  ELEMENTS_FOR_PAGE = CONFIG['media_elements_for_page_in_dashboard']
-  ELEMENT_PAGES = CONFIG['media_element_pages_in_dashboard']
-  
-  before_filter :initialize_layout
+  before_filter :initialize_layout, :initialize_pagination
   
   def index
-    @lessons = @current_user.suggested_lessons(LESSONS_FOR_PAGE * LESSON_PAGES)
+    @lessons = @current_user.suggested_lessons(@lessons_for_page * @lesson_pages)
     @lessons_emptied = Lesson.dashboard_emptied? @current_user.id
-    @media_elements = @current_user.suggested_media_elements(ELEMENTS_FOR_PAGE * ELEMENT_PAGES)
+    @media_elements = @current_user.suggested_media_elements(@media_elements_for_page * @media_element_pages)
     @media_elements_emptied = MediaElement.dashboard_emptied? @current_user.id
     render_js_or_html_index
+  end
+  
+  private
+  
+  def initialize_pagination
+    @lessons_for_page = CONFIG['lessons_for_page_in_dashboard']
+    @lesson_pages = CONFIG['lesson_pages_in_dashboard']
+    @media_elements_for_page = CONFIG['media_elements_for_page_in_dashboard']
+    @media_element_pages = CONFIG['media_element_pages_in_dashboard']
   end
   
 end
