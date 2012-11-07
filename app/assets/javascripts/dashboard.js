e09941cb63d53ae781160489accc158ce5a8deaf
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-11-07 16:57:51 +0100

    paginazione dashboard lezioni

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -17,3 +17,35 @@ function switchToSuggestedLessons() {
   $('#switch_to_lessons').addClass('current');
   $('#switch_to_media_elements').removeClass('current');
 }
+
+function changePageDashboardLessons(old_pos, pos, pages_amount) {
+  $('#suggested_lessons_' + (old_pos)).hide('fade', {}, 500, function() {
+    $(this).css('display', 'none');
+    $('#suggested_lessons_' + (pos)).css('display', 'block');
+    var substitute = "<span class=\"dots_pagination\"><span role=\"pages\"><a data-page=\"";
+    if(pos <= 1) {
+      substitute += ((pos - 1) + "\" role=\"disabled\"></a>");
+    } else {
+      substitute += ((pos - 1) + "\"></a>");
+    }
+    substitute += ("<a data-page=\"" + pos + "\" role=\"current\"></a>");
+    if(pos >= pages_amount) {
+      substitute += "<a data-page=\"" + (pos + 1) + "\" role=\"disabled\"></a>"
+    } else {
+      substitute += "<a data-page=\"" + (pos + 1) + "\"></a>"
+    }
+    substitute += "</span></span>";
+    $('#dashboard_lessons_pagination').html(substitute);
+    $(document).ready(function() {
+      var prevPage = function(prevPage) {
+        changePageDashboardLessons(pos, (pos - 1), pages_amount);
+        return true;
+      }
+      var nextPage = function(nextPage) {
+        changePageDashboardLessons(pos, (pos + 1), pages_amount);
+        return true;
+      }
+      new DotsPagination($('[role=pages]'), pages_amount, prevPage, nextPage);
+    });
+  });
+}
