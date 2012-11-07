a8f574c74828f7b655c8b10b2fe074f5b3f19dfa
Maurizio De Santis <m.desantis@morganspa.com>
2012-11-07 17:14:16 +0100

    rimozione match in favore di get, post e put

--- config/routes.rb
+++ config/routes.rb
@@ -1,61 +1,61 @@
 Desy::Application.routes.draw do
   
   # MAIN SECTIONS
-  match 'dashboard' => 'dashboard#index', :as => :dashboard_index
-  match 'lessons' => 'lessons#index', :as => :my_lessons
-  match 'media_elements' => 'media_elements#index', :as => :my_media_elements
-  match 'virtual_classroom' => 'virtual_classroom#index', :as => :my_virtual_classroom
+  get  'dashboard' => 'dashboard#index', :as => :dashboard_index
+  get  'lessons' => 'lessons#index', :as => :my_lessons
+  get  'media_elements' => 'media_elements#index', :as => :my_media_elements
+  get  'virtual_classroom' => 'virtual_classroom#index', :as => :my_virtual_classroom
   
   # ACTION BUTTONS FOR LESSONS
-  match 'lessons/:lesson_id/add' => 'lessons#add', :via => :post
-  match 'lessons/:lesson_id/copy' => 'lessons#copy', :via => :post
-  match 'lessons/:lesson_id/destroy' => 'lessons#destroy', :via => :post
-  match 'lessons/:lesson_id/dislike' => 'lessons#dislike', :via => :post
-  match 'lessons/:lesson_id/like' => 'lessons#like', :via => :post
-  match 'lessons/:lesson_id/publish' => 'lessons#publish', :via => :post
-  match 'lessons/:lesson_id/remove' => 'lessons#remove', :via => :post
-  match 'lessons/:lesson_id/unpublish' => 'lessons#unpublish', :via => :post
-  match 'virtual_classroom/:lesson_id/add_lesson' => 'virtual_classroom#add_lesson', :via => :post
-  match 'virtual_classroom/:lesson_id/remove_lesson' => 'virtual_classroom#remove_lesson', :via => :post
+  post 'lessons/:lesson_id/add' => 'lessons#add'
+  post 'lessons/:lesson_id/copy' => 'lessons#copy'
+  post 'lessons/:lesson_id/destroy' => 'lessons#destroy'
+  post 'lessons/:lesson_id/dislike' => 'lessons#dislike'
+  post 'lessons/:lesson_id/like' => 'lessons#like'
+  post 'lessons/:lesson_id/publish' => 'lessons#publish'
+  post 'lessons/:lesson_id/remove' => 'lessons#remove'
+  post 'lessons/:lesson_id/unpublish' => 'lessons#unpublish'
+  post 'virtual_classroom/:lesson_id/add_lesson' => 'virtual_classroom#add_lesson'
+  post 'virtual_classroom/:lesson_id/remove_lesson' => 'virtual_classroom#remove_lesson'
   
   # ACTION BUTTONS FOR MEDIA ELEMENTS
-  match 'media_elements/:media_element_id/add' => 'media_elements#add', :via => :post
-  match 'media_elements/:media_element_id/destroy' => 'media_elements#destroy', :via => :post
-  match 'media_elements/:media_element_id/remove' => 'media_elements#remove', :via => :post
-  match 'media_elements/:media_element_id/change_info' => 'media_elements#change_info', :via => :post, :as => :change_media_element_info
+  post 'media_elements/:media_element_id/add' => 'media_elements#add'
+  post 'media_elements/:media_element_id/destroy' => 'media_elements#destroy'
+  post 'media_elements/:media_element_id/remove' => 'media_elements#remove'
+  post 'media_elements/:media_element_id/change_info' => 'media_elements#change_info', :as => :change_media_element_info
   
   # NOTIFICATIONS
-  match 'notifications/:notification_id/seen' => 'notifications#seen', :via => :post
-  match 'notifications/:notification_id/destroy' => 'notifications#destroy', :via => :post
-  match 'notifications/get_new_block' => 'notifications#get_new_block'
+  post 'notifications/:notification_id/seen' => 'notifications#seen'
+  post 'notifications/:notification_id/destroy' => 'notifications#destroy'
+  get  'notifications/get_new_block' => 'notifications#get_new_block'
   
   # REPORTS
-  match 'reports/lesson' => 'reports#lesson', :via => :post
-  match 'reports/media_element' => 'reports#media_element', :via => :post
+  post 'reports/lesson' => 'reports#lesson'
+  post 'reports/media_element' => 'reports#media_element'
   
   # LESSON EDITOR
-  match 'lesson_editor/new' => 'lesson_editor#new'
-  match 'lesson_editor/create' => 'lesson_editor#create', :via => :post
-  match 'lesson_editor/:lesson_id/index' => 'lesson_editor#index', :as => :lesson_editor
-  match 'lesson_editor/:lesson_id/edit' => 'lesson_editor#edit', :as => :edit_lesson
-  match 'lesson_editor/:lesson_id/update' => 'lesson_editor#update', :as => :lesson, :via => :put
-  match 'lesson_editor/:lesson_id/add_slide/:position' => 'lesson_editor#add_slide', :via => :post
-  match 'lesson_editor/add_new_slide/:slide' => 'lesson_editor#add_new_slide', :as => :add_new_slide
-  match 'lesson_editor/show_gallery/:slide' => 'lesson_editor#show_gallery', :as => :show_gallery
+  get  'lesson_editor/new' => 'lesson_editor#new'
+  post 'lesson_editor/create' => 'lesson_editor#create'
+  get  'lesson_editor/:lesson_id/index' => 'lesson_editor#index', :as => :lesson_editor
+  get  'lesson_editor/:lesson_id/edit' => 'lesson_editor#edit', :as => :edit_lesson
+  put  'lesson_editor/:lesson_id/update' => 'lesson_editor#update', :as => :lesson
+  post 'lesson_editor/:lesson_id/add_slide/:position' => 'lesson_editor#add_slide'
+  get  'lesson_editor/add_new_slide/:slide' => 'lesson_editor#add_new_slide', :as => :add_new_slide
+  get  'lesson_editor/show_gallery/:slide' => 'lesson_editor#show_gallery', :as => :show_gallery
   
   # SEARCH LESSONS OR MEDIA ELEMENTS
-  match 'search' => 'search#index', :as => :search_items, :via => :get
+  get  'search' => 'search#index', :as => :search_items
   
   # LOGGED USER
-  match 'profile' => 'users#edit', :as => :my_profile
-  match 'profile/update' => 'users#update', :via => :post
-  match 'logout' => 'users#logout', :as => :logout
+  get  'profile' => 'users#edit', :as => :my_profile
+  post 'profile/update' => 'users#update'
+  get  'logout' => 'users#logout', :as => :logout
   
   # NOT LOGGED USER
-  match 'prelogin' => 'prelogin#index', :as => :prelogin
-  match 'login' => 'prelogin#login', :as => :login
-  match 'sign_up' => 'prelogin#registration', :as => :sign_up
-  match 'create_user' => 'prelogin#create_registration', :as => :create_user, :via => :post
+  get  'prelogin' => 'prelogin#index', :as => :prelogin
+  get  'login' => 'prelogin#login', :as => :login
+  get  'sign_up' => 'prelogin#registration', :as => :sign_up
+  post 'create_user' => 'prelogin#create_registration', :as => :create_user
   
   # APPLICATION ROOT
   root :to => 'prelogin#index'
