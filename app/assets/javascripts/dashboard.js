21868a3358f69d3a942dff949051fccba7475467
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-11-07 17:32:22 +0100

    sistemando altre cose dashboard

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,9 +1,6 @@
 function switchToSuggestedMediaElements() {
   $('#lessons_in_dashboard').css('display', 'none');
   $('#media_elements_in_dashboard').css('display', 'block');
-  $('.suggested_lessons_page').css('display', 'none');
-  $('.suggested_media_elements_page').css('display', 'none');
-  $('#suggested_media_elements_1').css('display', 'block');
   $('#switch_to_media_elements').addClass('current');
   $('#switch_to_lessons').removeClass('current');
 }
@@ -11,9 +8,6 @@ function switchToSuggestedMediaElements() {
 function switchToSuggestedLessons() {
   $('#media_elements_in_dashboard').css('display', 'none');
   $('#lessons_in_dashboard').css('display', 'block');
-  $('.suggested_lessons_page').css('display', 'none');
-  $('.suggested_media_elements_page').css('display', 'none');
-  $('#suggested_lessons_1').css('display', 'block');
   $('#switch_to_lessons').addClass('current');
   $('#switch_to_media_elements').removeClass('current');
 }
