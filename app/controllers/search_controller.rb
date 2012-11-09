63cd88b47a90a6bd948a774c27e1e2af16ef258e
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-11-09 17:18:49 +0100

    empty in search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -22,6 +22,7 @@ class SearchController < ApplicationController
           end
       end
     end
+    @tag = Tag.find_by_id(@tag.id) if @tag.class == Fixnum
     render_js_or_html_index
   end
   
