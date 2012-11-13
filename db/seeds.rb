c8b2e78d4950891101f41e5306e16e64ce5535ea
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-11-13 16:19:45 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,4 +28,18 @@ if Rails.env.development? && required
   plant_development_seeds
 end
 
-puts "Created #{Subject.count} subjects, #{Location.count} locations, #{SchoolLevel.count} school_levels, #{User.count} users, #{UsersSubject.count} users_subjects, #{Lesson.count} lessons, #{MediaElement.count} media_elements, #{Slide.count} slides, #{Notification.count} notifications, #{Like.count} likes, #{Bookmark.where(:bookmarkable_type => 'Lesson').count} bookmarks for lessons, #{Bookmark.where(:bookmarkable_type => 'MediaElement').count} bookmarks for media elements, #{Tag.count} tags, #{Tagging.count} taggings"
+puts "Created:"
+puts "#{Subject.count} subjects (should be 3)"
+puts "#{Location.count} locations (should be 3)"
+puts "#{SchoolLevel.count} school_levels (should be 3)"
+puts "#{User.count} users (should be 18)"
+puts "#{UsersSubject.count} users_subjects (should be 54)"
+puts "#{Lesson.count} lessons (should be 43)"
+puts "#{MediaElement.count} media_elements (should be 70)"
+puts "#{Slide.count} slides (should be 73)"
+puts "#{Notification.count} notifications (should be 42)"
+puts "#{Like.count} likes (should be 122)"
+puts "#{Bookmark.where(:bookmarkable_type => 'Lesson').count} bookmarks for lessons (should be 12)"
+puts "#{Bookmark.where(:bookmarkable_type => 'MediaElement').count} bookmarks for media elements (should be 17)"
+puts "#{Tag.count} tags (should be 34)"
+puts "#{Tagging.count} taggings (should be 791)"
