db7cbc23d76d900de8c8c7f55ba603803fd140e4
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-11-15 17:50:13 +0100

    drag

--- app/assets/javascripts/virtual_classroom.js
+++ app/assets/javascripts/virtual_classroom.js
@@ -1,9 +1,15 @@
 function initializeDraggableVirtualClassroomLesson(id) {
   $('#' + id).draggable({
     revert: true,
-    helper: 'clone',
-    stack: 'div'//,
-//    start: 
+    helper: function() {
+      var current_z_index = getMaximumZIndex('_virtual_classroom_lesson') + 1;
+      var div_to_return = $('#' + this.id + ' ._cover_slide_thumb')[0].outerHTML;
+      div_to_return = '<div ' + 'style="' + current_z_index + '" ' + div_to_return.substr(5,div_to_return.length);
+      return div_to_return;
+    },
+    start: function() {
+      $('#' + id + ' ._cover_slide_thumb').addClass('current');
+    }
   });
 }
 
@@ -11,4 +17,15 @@ function initializeDraggableVirtualClassroom() {
   $('._virtual_classroom_lesson').each(function() {
     initializeDraggableVirtualClassroomLesson(this.id);
   });
+}
+
+function getMaximumZIndex(a_class) {
+  var index_highest = 0;
+  $('.' + a_class).each(function() {
+    var index_current = parseInt($(this).css("zIndex"), 10);
+    if(index_current > index_highest) {
+        index_highest = index_current;
+    }
+  });
+  return index_highest;
 }
