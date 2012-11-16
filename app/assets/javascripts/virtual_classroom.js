d429194d89e0ff64d1ec28e52d97d3324440662c
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-11-16 09:50:48 +0100

    effetti grafici

--- app/assets/javascripts/virtual_classroom.js
+++ app/assets/javascripts/virtual_classroom.js
@@ -1,23 +1,27 @@
 function initializeDraggableVirtualClassroomLesson(id) {
-  $('#' + id).draggable({
-    revert: true,
-    handle: '._cover_slide_thumb',
-    cursor: 'move',
-    helper: function() {
-      var current_z_index = getMaximumZIndex('_virtual_classroom_lesson') + 1;
-      var div_to_return = $('#' + this.id + ' ._cover_slide_thumb')[0].outerHTML;
-      div_to_return = '<div ' + 'style="' + current_z_index + '" ' + div_to_return.substr(5,div_to_return.length);
-      return div_to_return;
-    },
-    start: function() {
-      $('#' + id + ' ._cover_slide_thumb').addClass('current');
-    },
-    stop: function(event, ui) {
-      if(!ui.helper.hasClass('_lesson_dropped')) {
-        $('#' + id + ' ._cover_slide_thumb').removeClass('current');
+  if($('#' + id).hasClass('ui-draggable')) {
+    $('#' + id).draggable('enable');
+  } else {
+    $('#' + id).draggable({
+      revert: true,
+      handle: '._cover_slide_thumb',
+      cursor: 'move',
+      helper: function() {
+        var current_z_index = getMaximumZIndex('_virtual_classroom_lesson') + 1;
+        var div_to_return = $('#' + this.id + ' ._cover_slide_thumb')[0].outerHTML;
+        div_to_return = '<div ' + 'style="' + current_z_index + '" ' + div_to_return.substr(5,div_to_return.length);
+        return div_to_return;
+      },
+      start: function() {
+        $('#' + id + ' ._cover_slide_thumb').addClass('current');
+      },
+      stop: function(event, ui) {
+        if(!ui.helper.hasClass('_lesson_dropped')) {
+          $('#' + id + ' ._cover_slide_thumb').removeClass('current');
+        }
       }
-    }
-  });
+    });
+  }
 }
 
 function initializeDraggableVirtualClassroom() {
