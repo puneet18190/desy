8383b2b0be5ccbbecd07352c54b757ef6ee72291
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-11-26 10:10:03 +0100

    token filtro lesson viewer

--- app/controllers/lesson_viewer_controller.rb
+++ app/controllers/lesson_viewer_controller.rb
@@ -1,15 +1,25 @@
 class LessonViewerController < ApplicationController
   
-#  before_filter 
+  skip_before_filter :authenticate, :only => :index
+  before_filter :skip_authenticate_user_if_token, :only => :index
   
   def index
-    @lesson = Lesson.find params[:lesson_id]
+    if !@ok
+      redirect_to '/dashboard'
+    end
     #TODO pass a parameter for the slide to redirect to after add_slide
     #@slide_to = params[:slide_position] if params[:slide_position]
   end
   
   def playlist
     @playlist = @current_user.playlist
+  end
+  
+  private
+  
+  def skip_authenticate_user_if_token
+    initialize_lesson
+    update_ok(@lesson.is_public || (logged_in? && session[:user_id].to_i == @lesson.user_id)) if @ok && @lesson.token != params[:token]
   end
   
 end
