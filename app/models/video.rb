97ab59f9f2098cee3595d43f00719fcf66672451
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-11-28 16:14:33 +0100

    previews

--- app/models/video.rb
+++ app/models/video.rb
@@ -58,49 +58,45 @@ class Video < MediaElement
     return nil if !hash.has_key?(:parameters) || !hash[:parameters].kind_of?(Array)
     resp_hash[:parameters] = []
     hash[:parameters].each do |p|
-      return nil if !p.kind_of?(Hash) || !p.has_key?(:component)
+      return nil if !p.kind_of?(Hash) || !p.has_key?(:component) || !COMPONENTS.include?(p[:components])
       case p[:component]
         when VIDEO_COMPONENT
           video = Video.get_media_element_from_hash(p, :video_id, user_id, 'Video')
           return nil if video.nil?
           return nil if !p.has_key?(:from) || !p[:from].kind_of?(Integer) || !p.has_key?(:until) || !p[:until].kind_of?(Integer)
           return nil if p[:from] < 0 || p[:until] > video.duration
+          resp_hash[:parameters] << {
+            :component => VIDEO_COMPONENT,
+            :video => video,
+            :from => p[:from],
+            :until => p[:until]
+          }
         when TEXT_COMPONENT
-          
+          return nil if !p.has_key?(:content) || !p.has_key?(:duration) || !p.has_key?(:background_color) || !p.has_key?(:text_color)
+          return nil if !p[:duration].kind_of?(Integer) || p[:duration] < 1 || !COLORS.include?(p[:background_color]) || !COLORS.include?(p[:text_color])
+          resp_hash[:parameters] << {
+            :component => TEXT_COMPONENT,
+            :content => p[:content].to_s,
+            :duration => p[:duration],
+            :background_color => COLORS[p[:background_color]],
+            :text_color => COLORS[p[:text_color]]
+          }
         when IMAGE_COMPONENT
-          
+          image = Video.get_media_element_from_hash(p, :image_id, user_id, 'Image')
+          return nil if image.nil?
+          return nil if !p.has_key?(:duration) || !p[:duration].kind_of?(Integer) || p[:duration] < 1
+          resp_hash[:parameters] << {
+            :component => IMAGE_COMPONENT,
+            :image => image,
+            :duration => p[:duration]
+          }
       end
     end
+    resp_hash
   end
   
   def self.get_media_element_from_hash(hash, key, user_id, my_sti_type)
     (hash.has_key?(key) && hash[key].kind_of?(Integer)) ? MediaElement.extract(hash[key], user_id, my_sti_type) : nil
   end
-  
-  
-#  {
-#    :initial_video_id => 6,
-#    :audio_id => nil,
-#    :parameters => [
-#      {
-#        :component => Video::VIDEO_COMPONENT,
-#        :video_id => 1,
-#        :from => 12,
-#        :until => 24
-#      },
-#      {
-#        :component => Video::TEXT_COMPONENT,
-#        :content => 'Titolo titolo titolo',
-#        :duration => 14,
-#        :background_color => :red,
-#        :text_color => :white
-#      },
-#      {
-#        :component => Video::IMAGE_COMPONENT,
-#        :image_id => 3,
-#        :duration => 2
-#      }
-#    ]
-#  }
   
 end
