30c00c20f72366c6c6ab21f959f75a5dd84d4903
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-11-29 18:47:26 +0100

    galleries 2

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -4,10 +4,12 @@ class GalleriesController < ApplicationController
   AUDIOS_FOR_PAGE = CONFIG['audios_for_page_in_gallery']
   VIDEOS_FOR_PAGE = CONFIG['videos_for_page_in_gallery']
   
+  # _show_image_gallery_in_lesson_editor
   def image_for_lesson_editor
     @images = @current_user.own_media_elements(1, IMAGES_FOR_PAGE, Filters::IMAGE)[:records]
   end
   
+  # _show_audio_gallery_in_lesson_editor
   def audio_for_lesson_editor
     @audios = @current_user.own_media_elements(1, AUDIOS_FOR_PAGE, Filters::AUDIO)[:records]
   end
