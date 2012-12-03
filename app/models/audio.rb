4579700f43c7f0aacca24fbc5a6b152ac8b108c2
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-12-03 09:45:44 +0100

    audio player funzionante!

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -2,12 +2,12 @@ class Audio < MediaElement
   
   def mp3 # FIXME temporaneo
     # media.url.mp3
-    "/media_elements/audio/#{self.id.to_s}/audio_mp3.mp3"
+    "/media_elements/audios/#{self.id.to_s}/audio_mp3.mp3"
   end
   
   def ogg # FIXME temporaneo
     # media.url.ogg
-    "/media_elements/audio/#{self.id.to_s}/audio_ogg.ogg"
+    "/media_elements/audios/#{self.id.to_s}/audio_ogg.ogg"
   end
   
   def duration # FIXME temporaneo
