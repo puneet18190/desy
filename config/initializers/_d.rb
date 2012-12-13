0ece0e44f38517f56429cefe52e11ee7ef5ccf35
Maurizio De Santis <m.desantis@morganspa.com>
2012-12-13 18:08:25 +0100

    commento riguardante la motivazione di dichiarare _d in produzione

--- config/initializers/_d.rb
+++ config/initializers/_d.rb
@@ -6,5 +6,6 @@ when 'development', 'test'
     puts "#{caller.first}: #{args.map(&:inspect).join(', ')}".yellow
   end
 else
+  # lo dichiaro anche in produzione sia mai che mi scappa di lasciarlo nel codice
   def _d; end
 end
