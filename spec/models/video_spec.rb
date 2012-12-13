0ab725bc8f919a30e900b4ecfd450960c10e626f
Maurizio De Santis <m.desantis@morganspa.com>
2012-12-13 18:06:02 +0100

    validazioni Video

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,71 +1,49 @@
 require 'spec_helper'
+
 describe Video, slow: true do
 
-  supported_formats = MEVSS::FORMATS
+  let(:media_folder)         { Rails.root.join('spec/support/samples') }
+  let(:valid_video_path)     { media_folder.join 'valid video.flv' }
+  let(:tmp_valid_video_path) { media_folder.join 'tmp.valid video.flv' }
+  let(:media)                { ActionDispatch::Http::UploadedFile.new(filename: File.basename(tmp_valid_video_path), tempfile: File.open(tmp_valid_video_path)) }
+  let(:user)                 { User.admin }
 
-  let(:user)          { User.admin }
-  let(:uploaded_path) { "#{MEVSS::SAMPLES_FOLDER}/in put.flv" }
-  let(:filename)      { File.basename(uploaded_path) }
-  let(:tempfile)      { File.open(uploaded_path) }
-  let(:uploaded)      { ActionDispatch::Http::UploadedFile.new(filename: filename, tempfile: tempfile) }
-  let(:output_folder) { "#{Rails.root}/public/media_elements/videos/test/#{model.id}" }
+  def saved
+    FileUtils.cp MEVSS::VALID_VIDEO, tmp_valid_video_path
+    subject.save!
+    subject
+  end
 
   subject do
-    described_class.new(title: 'title', description: 'description', tags: 'a,b,c,d,e', media: uploaded) do |video|
-      video.user_id = user.id
+    described_class.new(title: 'title', description: 'description', tags: 'a,b,c,d,e', media: media) do |video|
+      video.user = User.admin
     end
   end
-  
+
   describe '#save' do
+
+    before(:all) { saved.reload }
+
+    let(:video) { subject }
       
-    before(:all) do
-      FileUtils.cp MEVSS::VALID_VIDEO, uploaded_path
-      subject.save
-    end
-
-    let(:output_format) { "#{Rails.root}/public/media_elements/videos/test/#{subject.id}/in-put.%s" }
-
-    supported_formats.each do |format|
-      context "with #{format} format", format: format do
-        let(:format) { format }
-        def info(format)
-          @info ||= {}
-          @info[format] ||= MediaEditing::Video::Info.new(output_format % format)
-        end
-
-        it "creates a valid video" do
-          expect{ info(format) }.to_not raise_error
-        end
-
-        it "sets the expected #{format}_duration" do
-          subject.reload.send(:"#{format}_duration").should == info(format).duration
-        end
-
-      end
-    end
+    include_examples 'after saving a video with a valid not converted media'
 
   end
 
   describe '#destroy' do
 
-    subject do
-      described_class.new(title: 'title', description: 'description', tags: 'a,b,c,d,e', media: uploaded) do |video|
-        video.user_id = User.admin.id
-      end
-    end
-    let(:model) { subject }
+    before(:all) { saved.destroy }
 
-    before(:all) do
-      FileUtils.cp MEVSS::VALID_VIDEO, uploaded_path
-      subject.save
-      subject.destroy
-    end
+    let(:output_folder) { "#{Rails.root}/public/media_elements/videos/test/#{subject.id}" }
 
     it 'destroys the video folder' do
-      File.exists?(output_folder).should_not be_true
+      File.exists?(subject.media.absolute_folder).should_not be_true
     end
 
   end
 
+  after(:all) do
+    FileUtils.rm tmp_valid_video_path if File.exists? tmp_valid_video_path
+  end
 
 end
