03ff65bed50cb7c2741b03dd77d1e58712d3764f
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-12-18 12:57:52 +0100

    sortable iniziato

--- app/assets/javascripts/video_editor.js
+++ app/assets/javascripts/video_editor.js
@@ -3,6 +3,44 @@ function initializeVideoEditor() {
     autoReinitialise: true
   });
   calculateNewPositionGalleriesInVideoEditor();
+  $('#video_editor_timeline').sortable({
+    scroll: true,
+    handle: '._video_editor_component_hover',
+    axis: 'x',
+    cursor: 'move',
+    cancel: '._video_editor_component_menu'/*,
+    
+    helper: function(event, ui) {
+      var div_to_return = $($('#' + ui.attr('id'))[0].outerHTML);
+      div_to_return.addClass('current');
+      div_to_return = div_to_return[0].outerHTML;
+      var my_index = div_to_return.indexOf('<div class="_video_editor_component_menu');
+      var second_half_string = div_to_return.substring(my_index, div_to_return.length);
+      var my_second_index = my_index + second_half_string.indexOf('</div>') + 6;
+      return div_to_return.substring(0, (my_index - 1)) + div_to_return.substring((my_second_index + 1), div_to_return.length);
+    },
+    stop: function(event, ui) {
+      var previous = ui.item.prev();
+      var new_position = 0;
+      var old_position = ui.item.data('position');
+      if(previous.length == 0) {
+        new_position = 1;
+      } else {
+        var previous_item_position = previous.data('position');
+        if(old_position > previous_item_position) {
+          new_position = previous_item_position + 1;
+        } else {
+          new_position = previous_item_position;
+        }
+      }
+      if(old_position != new_position) {
+        $.ajax({
+          type: 'post',
+          url: '/virtual_classroom/' + ui.item.data('lesson-id') + '/playlist/' + new_position + '/change_position'
+        });
+      }
+    }*/
+  });
 }
 
 function calculateNewPositionGalleriesInVideoEditor() {
