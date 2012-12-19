eaf61675ab09583bf3dbc5e30fcaf67afc4f89c0
Adriano Di Lauro <a.dilauro@morganspa.com>
2012-12-19 11:43:00 +0100

    problema risolto

--- app/assets/javascripts/document_ready.js
+++ app/assets/javascripts/document_ready.js
@@ -788,7 +788,7 @@ $(document).ready(function() {
     var infos = $('#info_container');
     if($(this).hasClass('_replace_component')) {
       infos.data('replacing-component', true);
-      infos.data('current-component', 'video_component_' + father.data('position'));
+      infos.data('current-component', father.attr('id'));
     } else {
       infos.data('replacing-component', false);
     }
