03957f55cc2f90a93fbbccb844c5ba9e5d9f2913
Andrea D'Ippolito <adedip@gmail.com>
2012-12-20 12:22:12 +0100

    fully working image editor text via file

--- lib/media/image/editing/add_text_to_image.rb
+++ lib/media/image/editing/add_text_to_image.rb
@@ -1,37 +1,38 @@
 require 'media'
 require 'media/image'
 require 'media/image/editing'
-require 'mini_magick'
+require 'media/image/editing/cmd'
 
 module Media
   module Image
     module Editing
-      class AddTextToImage
+      class AddTextToImage < Cmd
         
         #img is a mini_magick object
         def initialize(img, color_hex, font_size, coordX, coordY, text_value)
           @img, @color_hex, @font_size, @coordX, @coordY, @text = img, color_hex, font_size, coordX, coordY, text_value
         end
         
-        def run
-          @img.combine_options do |c|
-            c.fill "#{@color_hex}"
-            c.stroke "none"
-            c.font "#{Rails.root.join('vendor/fonts/DroidSansFallback.ttf')}"
-            c.pointsize "#{@font_size}"
-            c.gravity 'NorthWest'
-            #c.annotate
-            c.draw "text #{@coordX.to_i},#{@coordY.to_i} #{shellescaped_text}"
-          end
+        private
+        
+        def cmd!
+        font = "#{Rails.root.join('vendor/fonts/DroidSansFallback.ttf')}"
+        %Q[ mogrify
+              -fill       #{@color_hex.to_s.shellescape}
+              -stroke     none
+              -font       #{font.to_s.shellescape}
+              -pointsize  #{@font_size.to_s.shellescape}
+              -gravity    NorthWest
+              -annotate   +#{@coordX.to_i.to_s.shellescape}+#{@coordY.to_i.to_s.shellescape} #{shellescaped_text}
+              #{@img.to_s.shellescape} ].squish
         end
         
-        private
         def shellescaped_text
           case @text
           when File, Tempfile
-            "@#{text.path.shellescape}"
+            "@#{@text.path.shellescape}"
           when Pathname
-            "@#{text.to_s.shellescape}"
+            "@#{@text.to_s.shellescape}"
           else
             @text.to_s.shellescape
           end
