41db62b4a08a017f757a64ce091d41611c2884f8
Maurizio De Santis <m.desantis@morganspa.com>
2012-12-21 11:58:38 +0100

    Media::Editing::Image::TextToImage

---
+++ spec/lib/media/image/editing/cmd/text_to_image_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+
+module Media
+  module Image
+    module Editing
+      class Cmd
+        describe TextToImage do
+          context 'with valid arguments' do
+            let(:text) do
+              Tempfile.new('desy_spec').tap do |f|
+                f.write("Test\ntest")
+                f.close
+              end
+            end
+            let(:output)  { Rails.root.join('tmp/test.jpg').to_s }
+            let(:options) { { color: '"#41A62A"' } }
+
+            before(:all) do
+              FileUtils.rm output if File.exists? output
+            end
+
+            it 'works' do
+              expect{ described_class.new(text, output, options).run! $stdout, $stderr }.to_not raise_error
+            end
+
+            after(:all) do
+              text.unlink
+              begin
+                FileUtils.rm output
+              rescue Errno::ENOENT
+              end
+            end
+            
+          end
+        end
+      end
+    end
+  end
+end
