18801ae78516c6a343c3b5834e4977a858092d8c
Maurizio De Santis <m.desantis@morganspa.com>
2013-01-09 17:36:55 +0100

    Seeds per production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,25 @@
-# encoding: UTF-8
+def create_locations_school_levels_subjects_admin_user
+  CONFIG['locations'].each do |l|
+    location = Location.new :description => l
+    location.save!
+  end
 
-CONFIG['locations'].each do |l|
-  location = Location.new :description => l
-  location.save!
+  CONFIG['school_levels'].each do |sl|
+    school_level = SchoolLevel.new :description => sl
+    school_level.save!
+  end
+
+  subject_ids = []
+  CONFIG['subjects'].each do |s|
+    subject = Subject.new :description => s
+    subject.save!
+    subject_ids << subject.id
+  end
+
+  user_name = CONFIG['admin_username'].split(' ').first
+  user_surname = CONFIG['admin_username'].gsub("#{user_name} ", '')
+  User.create_user(CONFIG['admin_email'], user_name, user_surname, 'School', SchoolLevel.first.id, Location.first.id, subject_ids)
 end
-
-CONFIG['school_levels'].each do |sl|
-  school_level = SchoolLevel.new :description => sl
-  school_level.save!
-end
-
-subject_ids = []
-CONFIG['subjects'].each do |s|
-  subject = Subject.new :description => s
-  subject.save!
-  subject_ids << subject.id
-end
-
-user_name = CONFIG['admin_username'].split(' ').first
-user_surname = CONFIG['admin_username'].gsub("#{user_name} ", '')
-User.create_user(CONFIG['admin_email'], user_name, user_surname, 'School', SchoolLevel.first.id, Location.first.id, subject_ids)
 
 begin
   require Rails.root.join("db/seeds/environments", Rails.env)
