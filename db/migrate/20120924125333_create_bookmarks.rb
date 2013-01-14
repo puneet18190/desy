b23528254531c55b1dc159531c8d06ebab54b79c
Maurizio De Santis <m.desantis@morganspa.com>
2013-01-14 11:05:52 +0100

    assegnazione manuale indice bookmarks in quanto il nome automatico risulta troppo lungo

--- db/migrate/20120924125333_create_bookmarks.rb
+++ db/migrate/20120924125333_create_bookmarks.rb
@@ -2,9 +2,9 @@ class CreateBookmarks < ActiveRecord::Migration
   
   def change
     create_table :bookmarks do |t|
-      t.integer :user_id, :null => false
-      t.integer :bookmarkable_id, :null => false, :references => nil
-      t.column :bookmarkable_type, :teaching_object, :null => false, :index => {:with => [:bookmarkable_id, :user_id], :unique => true}
+      t.integer :user_id, null: false
+      t.integer :bookmarkable_id, null: false, references: nil
+      t.column :bookmarkable_type, :teaching_object, null: false, index: { unique: true, with: [:bookmarkable_id, :user_id], name: 'index_bookmarks_on_bookmarkable_type_bookmarkable_id_user_id' }
       t.timestamps
     end
   end
