c6eeaf3d3f2fbe1cf792546367379a3b0502583e
Andrea D'Ippolito <adedip@gmail.com>
2013-01-17 15:39:56 +0100

    copy lesson fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def statistics
     @my_created_lessons = Statistics.my_created_lessons
     @my_created_elements = Statistics.my_created_elements
+    @my_copied_lessons = Statistics.my_copied_lessons
     @my_liked_lessons = Statistics.my_liked_lessons(3)
     @all_liked_lessons = Statistics.all_liked_lessons(3)
     @my_likes_count = Statistics.my_likes_count
