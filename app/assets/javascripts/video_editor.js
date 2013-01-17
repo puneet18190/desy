0256118ba91b536f521078629cd79a21ad3de75f
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-01-17 15:00:41 +0100

    sldgjbn

--- app/assets/javascripts/video_editor.js
+++ app/assets/javascripts/video_editor.js
@@ -605,6 +605,9 @@ function playVideoEditorComponent(component) {
         component.find('._video_component_transition').removeClass('current');
         next_component.find('._video_editor_component_hover, ._video_component_icon').removeClass('selected');
         $('#video_component_' + next_identifier + '_preview').show('fade', {}, 1000, function() {
+          if(!$('#video_editor_global_preview').data('in-use')) {
+            $('._video_component_transition').addClass('current');
+          }
           increaseVideoEditorPreviewTimer(false);
           component.find('._video_editor_component_hover, ._video_component_icon').addClass('selected');
           if($('#video_editor_global_preview').data('in-use')) {
