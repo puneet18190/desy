01bd842d6abcc6c687e339a7f5da09bae5383ffa
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-01-22 12:28:27 +0100

    image editor rifatto, parte 2

--- app/controllers/image_editor_controller.rb
+++ app/controllers/image_editor_controller.rb
@@ -17,8 +17,8 @@ class ImageEditorController < ApplicationController
   end
   
   def crop
-    if @ok
-      @crop_url = @image.crop
+    if @ok && !params[:x1].blank?
+      @crop_url = @image.crop params[:x1], params[:y1], params[:x2], params[:y2], current_user.id
     else
       @crop_url = ''
     end
@@ -26,47 +26,24 @@ class ImageEditorController < ApplicationController
   
   def save
     if @ok
-      
-      if !params[:cropped_image].blank?
-        editing_folder = File.join(@image.media.folder, "editing","user_#{current_user.id}")
-        image_path = File.join(editing_folder,params[:cropped_image])
-        img = MiniMagick::Image.open(image_path)
-        original_height = img[:height]
-        original_width = img[:width]
-      else
-        image_path = @image.url
-        img = MiniMagick::Image.open(image_path)
-        original_width = @image.media.width
-        original_height = @image.media.height
-      end
-      
       if !params[:x1].blank?
-        x1= ratio_value(woh[1],params[:x1],woh[0])
-        y1= ratio_value(woh[1],params[:y1],woh[0])
-        x2= ratio_value(woh[1],params[:x2],woh[0])
-        y2= ratio_value(woh[1],params[:y2],woh[0])
-        w = x2.to_i - x1.to_i
-        h = y2.to_i - y1.to_i
-        crop_params = "#{w}x#{h}+#{x1}+#{y1}"
-        img.crop(crop_params)
+        @image.crop params[:x1], params[:y1], params[:x2], params[:y2], current_user.id
       end
-            
-      new_image = Image.new { |me| me.user = current_user }
+      image_url = @image.process_textareas extract_textareas_params(params)
+      
+      
+      
+      new_image = Image.new
+      new_image.user_id = current_user.id
       new_image.title = params[:new_title]
       new_image.description = params[:new_description]
       new_image.tags = params[:new_tags]
-      
       new_image.valid?
-        msg = new_image.errors.messages
-        msg.delete(:media)
-      
+      msg = new_image.errors.messages
+      msg.delete(:media)
       if msg.empty?
-        in_tmpdir do |tmpdir|
-          new_filename = "#{params[:new_title].gsub(/[^0-9A-Za-z]/, '')}-edit-#{Time.now.strftime('%Y%m%d-%H%M%S')}.jpg"
-          new_file_path = File.join(tmpdir,new_filename)
-          img.write(new_file_path)
-          woh = width_or_height(original_width,original_height)
-          process_textareas(params,woh,new_file_path)
+          
+
           new_image.media = File.open(new_file_path)
           new_image.save
           remove_crop_path(@image)
@@ -119,7 +96,7 @@ class ImageEditorController < ApplicationController
           new_filename = "#{params[:new_title].gsub(/[^0-9A-Za-z]/, '')}-edit-#{Time.now.strftime('%Y%m%d-%H%M%S')}.jpg"
           img.write("#{tmpdir}/#{new_filename}")
           woh = width_or_height(original_width,original_height)
-          process_textareas(params,woh,"#{tmpdir}/#{new_filename}")
+          process_textareas extract_textareas(params)
           @image.media = File.open("#{tmpdir}/#{new_filename}")
           @image.save
           remove_crop_path(@image)
@@ -138,57 +115,26 @@ class ImageEditorController < ApplicationController
   
   private
   
-  def process_textareas(params, woh,image_path)
-    textCount = 0
+  def extract_textareas_params(params)
+    text_count = 0
+    resp = []
     params.each do |p,val|
       if p.starts_with?('text')
-        textCount += 1
+        text_count += 1
       end
     end
-
-    if textCount > 0
-      (0..textCount-1).each do |t_num|
-        
-        color_value = params["color_#{t_num}"]
-        color_hex = SETTINGS["colors"]["#{color_value.gsub('color_','')}"]["code"]
-        size_value = params["font_#{t_num}"].to_f
-        width_val = woh[1]
-        original_val = woh[0]
-        font_size = "#{ratio_value(width_val,(size_value), original_val)}"
-        coords_value = params["coords_#{t_num}"].to_s.split(",")
-        coordX = ratio_value(width_val,coords_value[0], original_val)
-        coordY = ratio_value(width_val,coords_value[1], original_val)
-        text_value = params["text_#{t_num}"]
-        
-        tmp_file = Tempfile.new('textarea')
-        begin
-          tmp_file.write("#{text_value}")
-          tmp_file.close
-          cmd = Media::Image::Editing::AddTextToImage.new(image_path, color_hex, font_size, coordX, coordY, tmp_file)
-          puts cmd
-          cmd.run!
-        ensure
-          tmp_file.unlink
-        end
+    if text_count > 0
+      (0..text_count-1).each do |t_num|
+        resp << {
+          :color => SETTINGS['colors'][params["color_#{t_num}"].gsub('color_', '')]['code'],
+          :font_size => params["font_#{t_num}"].to_f,
+          :coord_x => params["coords_#{t_num}"].split(',').first.to_f,
+          :coord_y => params["coords_#{t_num}"].split(',').last.to_f,
+          :text => params["text_#{t_num}"]
+        }
       end
     end
-  end
-  
-  def remove_crop_path(image)
-    editing_folder = File.join(image.media.folder, "editing","user_#{current_user.id}")
-    FileUtils.rm_rf(editing_folder) if File.exists?(editing_folder)
-  end
-  
-  def in_tmpdir
-    path = File.expand_path "#{Dir.tmpdir}/#{Time.now.to_i}#{rand(1000)}/"
-    FileUtils.mkdir_p path
-    yield path
-  ensure
-    FileUtils.rm_rf(path) if File.exists?(path)
-  end
-  
-  def remove_tmp_crop(image_dir)
-    #dir_contents = Dir.entries("#{image_dir}")
+    resp
   end
   
   def initialize_image_with_owner_or_public
@@ -202,7 +148,5 @@ class ImageEditorController < ApplicationController
     @image = Image.find_by_id @image_id
     update_ok(!@image.nil? && !@image.is_public && current_user.id == @image.user_id)
   end
-  
-  
   
 end
