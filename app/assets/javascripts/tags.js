06bcdbc7ecc2b04437fcbcceea2f40506202e6cb
Andrea D'Ippolito <adedip@gmail.com>
2013-01-24 11:11:22 +0100

    tags

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,57 +1,73 @@
-
 function initTagsAutocomplete(div_id){
-  $(function() {
-      function split( val ) {
-        return val.split( /,\s*/ );
-      }
-      function extractLast( term ) {
-        return split( term ).pop();
-      }
-
-      $( "#"+div_id )
-        // don't navigate away from the field on tab when selecting an item
-        .bind( "keydown", function( event ) {
-          if ( event.keyCode === $.ui.keyCode.TAB &&
-              $( this ).data( "autocomplete" ).menu.active ) {
-            event.preventDefault();
-          }
-        })
-        .autocomplete({
-          source: function( request, response ) {
-            $.getJSON( "/lessons_editor/get_tag_list", {
-              term: extractLast( request.term )
-            }, response );
-          },
-          search: function() {
-            // custom minLength
-            var term = extractLast( this.value );
-            if ( term.length < 2 ) {
-              return false;
-            }
-          },
-          open: function( event, ui ) {
-            var h = $("#autocomplete-selected").outerHeight();
-            var p = $("#tags").position().top;
-            console.log(h+"-"+p);
-            $(".ui-autocomplete").css('top', (130+p+h)+"px");
-            $(".ui-autocomplete").css('left','668px');
-            $(".ui-autocomplete").css('width','319px');
-          },
-          focus: function() {
-            // prevent value inserted on focus
-            return false;
-          },
-          select: function( event, ui ) {
-            var terms = split( this.value );
-            // remove the current input
-            terms.pop();
-            // add the selected item
-            terms.push( ui.item.value );
-            // add placeholder to get the comma-and-space at the end
-            terms.push( "" );
-            this.value = terms.join( ", " );
-            return false;
-          }
-        });
-    });
+  function split( val ) {
+    return val.split( /,\s*/ );
+  }
+  function extractLast( term ) {
+    return split( term ).pop();
+  }
+  
+  
+  $(function(){
+		
+		//attach autocomplete
+		$("#"+div_id).autocomplete({
+			
+			//define callback to format results
+			source: function( request, response ) {
+        $.getJSON( "/lessons_editor/get_tag_list", {
+          term: extractLast( request.term )
+        }, response );
+      },
+      search: function() {
+        // custom minLength
+        var term = extractLast( this.value );
+        if ( term.length < 2 ) {
+          return false;
+        }
+      },
+						
+			//define select handler
+			select: function(e, ui) {
+				
+				//create formatted friend
+				var friend = ui.item.value,
+					span = $("<span>").text(friend),
+					a = $("<a>").addClass("remove").attr({
+						href: "javascript:",
+						title: "Remove " + friend
+					}).text("x").appendTo(span);
+				
+				//add friend to friend div
+				console.log($("#"+div_id).val())
+				span.insertBefore("#"+div_id);
+				$("#"+div_id).val("").css("top", 2);
+			},
+			
+			//define select handler
+			change: function() {
+				
+				//prevent 'to' field being updated and correct position
+				$("#"+div_id).val("").css("top", 2);
+			}
+		});
+		
+		//add click handler to new-lesson-tags div
+		$("#new-lesson-tags").click(function(){
+			
+			//focus 'to' field
+			$("#"+div_id).focus();
+		});
+		
+		//add live handler for clicks on remove links
+		$(".remove", document.getElementById("new-lesson-tags")).live("click", function(){
+		
+			//remove current friend
+			$(this).parent().remove();
+			
+			//correct 'to' field position
+			if($("#new-lesson-tags span").length === 0) {
+				$("#"+div_id).css("top", 0);
+			}				
+		});				
+	});
 }
