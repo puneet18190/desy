e80f22e87c7a82846272ce6a68d61d6513ccfd33
Aurelia Mangione <a.mangione@morganspa.com>
2013-01-31 12:52:14 +0100

    Rifacimento totale layout popup virtual classroom

--- app/assets/javascripts/document_ready.js
+++ app/assets/javascripts/document_ready.js
@@ -521,7 +521,10 @@ $(document).ready(function() {
   $('#notifications_list').jScrollPane({
     autoReinitialise: true
   });
-  
+  $("#select_mailing_list").selectbox();
+
+  $("#select_lesson_list").selectbox();
+
   $("#which_item_to_search").selectbox();
   
   $("#filter_lessons").selectbox();
