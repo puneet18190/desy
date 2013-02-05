c9fd0833bdf5f42611192c560c8c5ba4c52777f5
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-02-05 16:31:34 +0100

    galleries.js

--- app/assets/javascripts/galleries.js
+++ app/assets/javascripts/galleries.js
@@ -1,25 +1,52 @@
-function initializeImageGallery() {
-  $('#image_gallery_content > div').jScrollPane({
+// funzioni di inizializzazione standard
+
+function initializeImageGalleryInLessonEditor() {
+  $('#lesson_editor_image_gallery_container #image_gallery_content > div').jScrollPane({
     autoReinitialise: true
   });
 }
 
-function initializeAudioGallery() {
-  $('#audio_gallery_content > div').jScrollPane({
+function initializeAudioGalleryInLessonEditor() {
+  $('#lesson_editor_audio_gallery_container #audio_gallery_content > div').jScrollPane({
     autoReinitialise: true
   });
 }
 
-function initializeVideoGallery() {
-  $('#video_gallery_content > div').jScrollPane({
+function initializeVideoGalleryInLessonEditor() {
+  $('#lesson_editor_video_gallery_container #video_gallery_content > div').jScrollPane({
     autoReinitialise: true
   });
 }
 
 function initializeVideoEditorMixedGallery() {
-  initializeVideoGallery();
-  initializeImageGallery();
+  $('#video_editor_mixed_gallery_container #video_gallery_content > div').jScrollPane({
+    autoReinitialise: true
+  });
+  $('#video_editor_mixed_gallery_container #image_gallery_content > div').jScrollPane({
+    autoReinitialise: true
+  });
 }
+
+function initializeAudioGalleryInVideoEditor() {
+  $('#video_editor_audio_gallery_container #audio_gallery_content > div').jScrollPane({
+    autoReinitialise: true
+  });
+}
+
+function initializeAudioGalleryInAudioEditor() {
+  $('#audio_editor_gallery_container #audio_gallery_content > div').jScrollPane({
+    autoReinitialise: true
+  });
+}
+
+function initializeImageGalleryInImageEditor() {
+  $('#image_gallery_for_image_editor #image_gallery_content > div').jScrollPane({
+    autoReinitialise: true
+  });
+}
+
+// fino a qui
+
 
 function resizedWidthForImageGallery(width, height) {
   if(height > width) {
