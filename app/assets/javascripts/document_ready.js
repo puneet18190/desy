db00f72305ef3d164f660b72f78da7438456e27c
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-02-07 10:20:37 +0100

    piccole correzioni

--- app/assets/javascripts/document_ready.js
+++ app/assets/javascripts/document_ready.js
@@ -297,29 +297,23 @@ $(document).ready(function() {
   
   $('body').on('click', '._close_audio_gallery_in_video_editor', function() {
     closeGalleryInVideoEditor('audio');
-    var audio_id = 0;
-    $('._audio_gallery_thumb').each(function() {
-      if($(this).find('._expanded').css('display') == 'block') {
-        audio_id = $(this).find('._add_audio_track_to_video_editor').data('audio-id');
-      }
-    });
-    if(audio_id != 0) {
-      stopMedia('#gallery_audio_' + audio_id + ' audio');
-      $('#gallery_audio_' + audio_id + ' ._expanded').hide();
+    var expanded_audio = $('._audio_expanded_in_gallery');
+    if(expanded_audio.length > 0) {
+      expanded_audio.removeClass('_audio_expanded_in_gallery');
+      var audio_id = expanded_audio.attr('id');
+      stopMedia('#' + audio_id + ' audio');
+      $('#' + audio_id + ' ._expanded').hide();
     }
   });
   
   $('body').on('click', '._close_audio_gallery_in_audio_editor', function() {
     closeGalleryInAudioEditor();
-    var audio_id = 0;
-    $('._audio_gallery_thumb').each(function() {
-      if($(this).find('._expanded').css('display') == 'block') {
-        audio_id = $(this).find('._add_audio_component_to_audio_editor').data('audio-id');
-      }
-    });
-    if(audio_id != 0) {
-      stopMedia('#gallery_audio_' + audio_id + ' audio');
-      $('#gallery_audio_' + audio_id + ' ._expanded').hide();
+    var expanded_audio = $('._audio_expanded_in_gallery');
+    if(expanded_audio.length > 0) {
+      expanded_audio.removeClass('_audio_expanded_in_gallery');
+      var audio_id = expanded_audio.attr('id');
+      stopMedia('#' + audio_id + ' audio');
+      $('#' + audio_id + ' ._expanded').hide();
     }
   });
   
