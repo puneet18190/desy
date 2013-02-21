899ef59f81cecff2b064aa10bf8553cb13034a78
Andrea D'Ippolito <adedip@gmail.com>
2013-02-21 18:12:28 +0100

    admin search form init complete (p2)

--- app/models/admin_search_form.rb
+++ app/models/admin_search_form.rb
@@ -1,30 +1,32 @@
 class AdminSearchForm < Form
 
-  attr_accessor :search_type, :id, :keyword, :user_id, :subject_id, :element_type, :date_range_field, :date_from, :date_to,:province_id, :town_id, :school_id
+  attr_accessor :search_type, :id, :keyword, :user_id, :subject_id, :element_type, :date_range_field, :from, :to,:province_id, :town_id, :school_id
   
-  validates_numericality_of :id, :user_id, :subject_id, :province_id, :town_id, :school_id
+  validates_numericality_of :id, :unless => Proc.new {|c| c.id.blank?}
   
-  def self.search(search)
-    @asf = AdminSearchForm.new(search)
-    if @asf.valid? && search[:search_type].present?
-      case search[:search_type]
-        when 'lessons'
-          self.search_lessons(search)
-        when 'elements'
-          self.search_elements(search)
-        when 'users'
-          self.search_users(search)
+  def self.search(params, search_type)
+    if params
+      _d params
+      asf = new(params)
+      if asf.valid?
+        case search_type.to_s
+        when 'lessons', 'elements', 'users'
+          send :"search_#{search_type}", params
         else
           return false
+        end
+      else
+        return false
       end
-    else
-      return false
     end
   end
   
-  def self.search_lessons(search)
-    
+  def self.search_lessons(params)
+    lessons = Lesson.order('id DESC')
+    lessons = lessons.where(id: params[:id]) if params[:id].present?
+    lessons = lessons.where('title ILIKE ?', "%#{params[:keyword]}%") if params[:keyword].present?
+    lessons = lessons.where(subject_id: params[:subject_id]) if params[:subject_id].present?
+    lessons
   end
-  
-  
+    
 end
--- lib/autoload/form.rb
+++ lib/autoload/form.rb
@@ -1,7 +1,13 @@
 class Form
   include ActiveModel::Validations  
   include ActiveModel::Conversion  
-  extend ActiveModel::Naming  
+  extend ActiveModel::Naming
+  
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end  
   
   def persisted?
     false
