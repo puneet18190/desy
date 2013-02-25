8fdc336a76b4cdb4e972862cc88bc763025295dd
Andrea D'Ippolito <adedip@gmail.com>
2013-02-25 17:46:00 +0100

    admin users index page

--- app/models/admin_search_form.rb
+++ app/models/admin_search_form.rb
@@ -1,6 +1,6 @@
 class AdminSearchForm < Form
 
-  attr_accessor :search_type, :ordering, :id, :title, :keyword, :user, :subject_id, :sti_type, :element_type, :date_range_field, :from, :to,:province_id, :town_id, :school_id
+  attr_accessor :search_type, :ordering, :id, :title, :keyword, :user, :subject_id, :sti_type, :element_type, :date_range_field, :from, :to,:province_id, :town_id, :school_id, :school_level_id
   
   validates_numericality_of :id, :unless => Proc.new {|c| c.id.blank?}
   validates_presence_of :ordering
@@ -70,9 +70,13 @@ class AdminSearchForm < Form
     lessons = lessons.where(id: params[:id]) if params[:id].present?
     lessons = lessons.where('name ILIKE ? OR surname ILIKE ? OR email ILIKE ?', "%#{params[:user]}%" , "%#{params[:user]}%", "%#{params[:user]}%") if params[:user].present?
     lessons = lessons.where('location_id' => params[:school_id]) if params[:school_id].present?
+    lessons = lessons.where('school_level_id' => params[:school_level_id]) if params[:school_level_id].present?
     if params[:date_range_field].present? && params[:from].present? && params[:to].present?
       date_range = (Date.strptime(params[:from], '%d-%m-%Y').beginning_of_day)..(Date.strptime(params[:to], '%d-%m-%Y').end_of_day)
       lessons = lessons.where("#{params[:date_range_field]}" => date_range)
+    end
+    if params[:subject_id]
+      lessons = lessons.joins(:subjects)
     end
     lessons
   end
