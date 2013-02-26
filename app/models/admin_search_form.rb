93698b1934a2699fe0cd25911e90a084f7157cf6
Andrea D'Ippolito <adedip@gmail.com>
2013-02-26 10:36:31 +0100

    admin elements index complete grap_apart

--- app/models/admin_search_form.rb
+++ app/models/admin_search_form.rb
@@ -68,10 +68,20 @@ class AdminSearchForm < Form
       date_range = (Date.strptime(params[:from], '%d-%m-%Y').beginning_of_day)..(Date.strptime(params[:to], '%d-%m-%Y').end_of_day)
       elements = elements.where("#{params[:date_range_field]}" => date_range)
     end
-    if params[:school_id] || params[:user]
+    if params[:school_id] || params[:user] || params[:province_id] || params[:town_id]
       elements = elements.joins(:user)
     end
-    elements = elements.where('users.location_id' => params[:school_id]) if params[:school_id].present?
+    
+    if params[:school_id].present?
+      elements = elements.where('users.location_id' => params[:school_id])
+    elsif params[:town_id].present?
+      town = Location.find(params[:town_id])
+      elements = elements.where('users.location_id' => town.descendant_ids)
+    elsif params[:province_id].present?
+      province = Location.find(params[:province_id])
+      elements = elements.where('users.location_id' => province.descendant_ids)
+    end
+    
     elements = elements.where('users.name ILIKE ? OR users.surname ILIKE ?', "%#{params[:user]}%", "%#{params[:user]}%") if params[:user].present?
     elements
   end
