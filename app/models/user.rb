914ca74850847e3a686f3aedfcd4ce1e99fb86fb
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-02-28 14:39:07 +0100

    finito anche per media elements

--- app/models/user.rb
+++ app/models/user.rb
@@ -524,16 +524,26 @@ class User < ActiveRecord::Base
     limit = SETTINGS['tags_limit_in_search_engine']
     filter = Filters::ALL_MEDIA_ELEMENTS if filter.nil? || !Filters::MEDIA_ELEMENTS_SEARCH_SET.include?(filter)
     resp = []
-    where = 'tags.word LIKE ? AND (media_elements.is_public = ? OR media_elements.user_id = ?)'
+    where = 'tags.word != ? AND tags.word LIKE ? AND (media_elements.is_public = ? OR media_elements.user_id = ?)'
+    where_for_current_tag = 'tags.word = ? AND (media_elements.is_public = ? OR media_elements.user_id = ?)'
+    joins = "INNER JOIN tags ON (tags.id = taggings.tag_id) INNER JOIN media_elements ON (taggings.taggable_type = 'MediaElement' AND taggings.taggable_id = media_elements.id)"
+    select = 'tags.id AS tag_id, (SELECT COUNT(*) FROM taggings WHERE (taggings.tag_id = tags.id)) AS tags_count'
     case filter
       when Filters::VIDEO
         where = "#{where} AND media_elements.sti_type = 'Video'"
+        where_for_current_tag = "#{where_for_current_tag} AND media_elements.sti_type = 'Video'"
       when Filters::AUDIO
         where = "#{where} AND media_elements.sti_type = 'Audio'"
+        where_for_current_tag = "#{where_for_current_tag} AND media_elements.sti_type = 'Audio'"
       when Filters::IMAGE
         where = "#{where} AND media_elements.sti_type = 'Image'"
+        where_for_current_tag = "#{where_for_current_tag} AND media_elements.sti_type = 'Image'"
     end
-    Tagging.group('tags.id').select('tags.id AS tag_id').joins("INNER JOIN tags ON (tags.id = taggings.tag_id) INNER JOIN media_elements ON (taggings.taggable_type = 'MediaElement' AND taggings.taggable_id = media_elements.id)").where(where, "#{word}%", true, self.id).order('tags.word ASC').limit(limit).each do |tagging|
+    if Tagging.joins(joins).where(where_for_current_tag, word, true, self.id).limit(1).length > 0
+      resp << Tag.find_by_word(word)
+      limit -= 1
+    end
+    Tagging.group('tags.id').select(select).joins(joins).where(where, word, "#{word}%", true, self.id).order('tags_count DESC, tags.word ASC').limit(limit).each do |tagging|
       resp << Tag.find(tagging.tag_id)
     end
     resp
