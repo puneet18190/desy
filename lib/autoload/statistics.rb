31813df2fb4f90974ab03d72c87eb3969629e6dc
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-03-05 17:51:39 +0100

    migliorando le statistiche

--- lib/autoload/statistics.rb
+++ lib/autoload/statistics.rb
@@ -4,58 +4,79 @@ module Statistics
     
     attr_accessor :user
     
-    def my_created_lessons
-      Lesson.where(user_id: user.id, copied_not_modified: false)
-    end
     
-    def my_created_elements
-      MediaElement.where(:user_id => user.id)
-    end
+    # PART 1: statistics retrieving ActiveRecord arrays
     
-    def my_copied_lessons
-      Lesson.joins('INNER JOIN lessons AS parent_lessons ON (parent_lessons.id = lessons.parent_id)').where('lessons.user_id != ? AND parent_lessons.user_id = ?', user.id, user.id).count
-    end
-    
+    # The first n lessons of the current user, ordered by the number of likes received
     def my_liked_lessons(first_n)
-      Lesson.where(user_id: user.id, copied_not_modified: false).joins(:likes).group('lessons.id').order('count(likes) DESC').limit(first_n)
+      
     end
     
-    def my_likes_count
-      Lesson.where(user_id: user.id, copied_not_modified: false).joins(:likes).count
-    end
-    
+    # The first n lessons liked by users in all DESY
     def all_liked_lessons(first_n)
       Lesson.joins(:likes).group('lessons.id').order('count(likes) DESC').limit(first_n)
     end
     
-    def all_users
-      User.count
-    end
-    
-    def all_users_like
-      User.joins(:lessons => :likes).group('users.id').order('count(likes) DESC').count()
-    end
-    
-    def all_shared_lessons
-      Lesson.where(is_public: true).count
-    end
-    
-    def all_shared_elements
-      MediaElement.where(is_public: true).count
-    end
-    
+    # The first n users who received more likes
     def all_liked_users(first_n)
       Lesson.where(user_id: user.id).joins(:likes).group('lessons.id').order('count(likes) DESC').limit(first_n)
     end
     
+    # The first n users who received more likes throughout the application
+    def all_users_like(first_n)
+      
+    end
+    
+    
+    # PART 2: statistics retrieving numbers
+    
+    # Lessons created by the current user, and copied by other users
+    def my_copied_lessons
+      Lesson.joins('INNER JOIN lessons AS parent_lessons ON (parent_lessons.id = lessons.parent_id)').where('lessons.user_id != ? AND parent_lessons.user_id = ?', user.id, user.id).count
+    end
+    
+    # Lessons created by the current user (not including the lessons he copied and still not modified)
+    def my_created_lessons
+      Lesson.where(:user_id => user.id, :copied_not_modified => false).count
+    end
+    
+    # Media Elements loaded or created by the current user (including the public ones, that might not be anymore in the user's section)
+    def my_created_elements
+      MediaElement.where(:user_id => user.id).count
+    end
+    
+    # The total amount of likes received by lessons created by the current user
+    def my_likes_count
+      Like.joins(:lesson).where(:lesson => {:user_id => user.id}).count
+    end
+    
+    # The number of users in DESY
+    def all_users
+      User.count
+    end
+    
+    # The number of shared lessons in the application
+    def all_shared_lessons
+      Lesson.where(:is_public => true).count
+    end
+    
+    # The number of shared media elements in the application
+    def all_shared_elements
+      MediaElement.where(:is_public => true).count
+    end
+    
+    
+    # PART 3: statistics retrieving charts
+    
+    # Chart representing the distribution of subjects among lessons
     def all_subjects_chart
-      amt = ""
-      desc = ""
+      amt = ''
+      desc = ''
       Subject.all.each do |sbj|
         if sbj.lessons.count > 0
-          amt = amt + sbj_to_percentage(sbj.lessons.count).to_s + ","
+          amt = amt + sbj_to_percentage(sbj.lessons.count).to_s + ','
         end
-        desc = desc + sbj.description.to_s + ","
+        desc = desc + sbj.description.to_s + ','
       end
       return [amt.to_s[0..-2], desc.to_s[0..-2]]
     end
---
+++ test/unit/statistics_test.rb
@@ -0,0 +1,261 @@
+require 'test_helper'
+
+class StatisticsTest < ActiveSupport::TestCase
+  
+  def load_likers
+    @liker1 = User.confirmed.new(:password => 'em1@em.em', :password_confirmation => 'em1@em.em', :name => 'dgdsg', :surname => 'sdgds', :school_level_id => 1, :location_id => 1, :subject_ids => [1]) do |user|
+      user.email = 'em1@em.em'
+      user.active = true
+    end
+    @liker1.policy_1 = '1'
+    @liker1.policy_2 = '1'
+    assert @liker1.save
+    @liker2 = User.confirmed.new(:password => 'em2@em.em', :password_confirmation => 'em2@em.em', :name => 'dgdsg', :surname => 'sdgds', :school_level_id => 1, :location_id => 1, :subject_ids => [1]) do |user|
+      user.email = 'em2@em.em'
+      user.active = true
+    end
+    @liker2.policy_1 = '1'
+    @liker2.policy_2 = '1'
+    assert @liker2.save
+    @liker3 = User.confirmed.new(:password => 'em3@em.em', :password_confirmation => 'em3@em.em', :name => 'dgdsg', :surname => 'sdgds', :school_level_id => 1, :location_id => 1, :subject_ids => [1]) do |user|
+      user.email = 'em3@em.em'
+      user.active = true
+    end
+    @liker3.policy_1 = '1'
+    @liker3.policy_2 = '1'
+    assert @liker3.save
+    @liker4 = User.confirmed.new(:password => 'em4@em.em', :password_confirmation => 'em4@em.em', :name => 'dgdsg', :surname => 'sdgds', :school_level_id => 1, :location_id => 1, :subject_ids => [1]) do |user|
+      user.email = 'em4@em.em'
+      user.active = true
+    end
+    @liker4.policy_1 = '1'
+    @liker4.policy_2 = '1'
+    assert @liker4.save
+    @liker5 = User.confirmed.new(:password => 'em5@em.em', :password_confirmation => 'em5@em.em', :name => 'dgdsg', :surname => 'sdgds', :school_level_id => 1, :location_id => 1, :subject_ids => [1]) do |user|
+      user.email = 'em5@em.em'
+      user.active = true
+    end
+    @liker5.policy_1 = '1'
+    @liker5.policy_2 = '1'
+    assert @liker5.save
+    @liker6 = User.confirmed.new(:password => 'em6@em.em', :password_confirmation => 'em6@em.em', :name => 'dgdsg', :surname => 'sdgds', :school_level_id => 1, :location_id => 1, :subject_ids => [1]) do |user|
+      user.email = 'em6@em.em'
+      user.active = true
+    end
+    @liker6.policy_1 = '1'
+    @liker6.policy_2 = '1'
+    assert @liker6.save
+    @liker7 = User.confirmed.new(:password => 'em7@em.em', :password_confirmation => 'em7@em.em', :name => 'dgdsg', :surname => 'sdgds', :school_level_id => 1, :location_id => 1, :subject_ids => [1]) do |user|
+      user.email = 'em7@em.em'
+      user.active = true
+    end
+    @liker7.policy_1 = '1'
+    @liker7.policy_2 = '1'
+    assert @liker7.save
+    @liker8 = User.confirmed.new(:password => 'em8@em.em', :password_confirmation => 'em8@em.em', :name => 'dgdsg', :surname => 'sdgds', :school_level_id => 1, :location_id => 1, :subject_ids => [1]) do |user|
+      user.email = 'em8@em.em'
+      user.active = true
+    end
+    @liker8.policy_1 = '1'
+    @liker8.policy_2 = '1'
+    assert @liker8.save
+    @liker9 = User.confirmed.new(:password => 'em9@em.em', :password_confirmation => 'em9@em.em', :name => 'dgdsg', :surname => 'sdgds', :school_level_id => 1, :location_id => 1, :subject_ids => [1]) do |user|
+      user.email = 'em9@em.em'
+      user.active = true
+    end
+    @liker9.policy_1 = '1'
+    @liker9.policy_2 = '1'
+    assert @liker9.save
+  end
+  
+  def load_likes
+    load_likers
+    Like.all.each do |l|
+      l.destroy
+    end
+    assert @liker1.like @les1.id
+    assert @liker1.like @les3.id
+    assert @liker1.like @les6.id
+    assert @liker1.like @les9.id
+    assert @liker2.like @les2.id
+    assert @liker2.like @les3.id
+    assert @liker2.like @les4.id
+    assert @liker2.like @les7.id
+    assert @liker3.like @les1.id
+    assert @liker4.like @les4.id
+    assert @liker4.like @les8.id
+    assert @liker5.like @les5.id
+    assert @liker5.like @les6.id
+    assert @liker5.like @les7.id
+    assert @liker6.like @les8.id
+    assert @liker6.like @les1.id
+    assert @liker6.like @les3.id
+    assert @liker7.like @les1.id
+    assert @liker7.like @les5.id
+    assert @liker7.like @les6.id
+    assert @liker8.like @les8.id
+    assert @liker9.like @les8.id
+    assert @liker9.like @les1.id
+  end
+  
+  def load_items
+    @user1 = User.find 1
+    @user1.name = 'a_name'
+    @user1.surname = 'a_surname'
+    assert_obj_saved @user1
+    us_sub_1_2 = UsersSubject.new
+    us_sub_1_2.user_id = 1
+    us_sub_1_2.subject_id = 2
+    assert_obj_saved us_sub_1_2
+    us_sub_1_4 = UsersSubject.new
+    us_sub_1_4.user_id = 1
+    us_sub_1_4.subject_id = 4
+    assert_obj_saved us_sub_1_4
+    us_sub_1_5 = UsersSubject.new
+    us_sub_1_5.user_id = 1
+    us_sub_1_5.subject_id = 5
+    assert_obj_saved us_sub_1_5
+    us_sub_1_6 = UsersSubject.new
+    us_sub_1_6.user_id = 1
+    us_sub_1_6.subject_id = 6
+    assert_obj_saved us_sub_1_6
+    @user2 = User.find 2
+    @user2.name = 'a_name'
+    @user2.surname = 'a_surname'
+    assert_obj_saved @user2
+    us_sub_2_2 = UsersSubject.new
+    us_sub_2_2.user_id = 2
+    us_sub_2_2.subject_id = 2
+    assert_obj_saved us_sub_2_2
+    us_sub_2_3 = UsersSubject.new
+    us_sub_2_3.user_id = 2
+    us_sub_2_3.subject_id = 3
+    assert_obj_saved us_sub_2_3
+    us_sub_2_4 = UsersSubject.new
+    us_sub_2_4.user_id = 2
+    us_sub_2_4.subject_id = 4
+    assert_obj_saved us_sub_2_4
+    @user1 = User.find 1
+    @user2 = User.find 2
+    Tagging.all.each do |t|
+      ActiveRecord::Base.connection.execute "DELETE FROM taggings WHERE id = #{t.id}"
+    end
+    Tag.all.each do |t|
+      t.destroy
+    end
+    tag_map = {
+      0 => "cane, sole, togatto, cincillà, walter nudo, luna, di escrementi di usignolo",
+      1 => "walter nudo, luna, di escrementi di usignolo, disabili, torriere architettoniche, mare, petrolio",
+      2 => "torriere architettoniche, mare, petrolio, sostenibilità, di immondizia, tonquinamento atmosferico, tonquinamento",
+      3 => "tonquinamento, pollution, tom cruise, cammello, cammelli, acqua, acquario",
+      4 => "cammelli, acqua, acquario, acquatico, 個名, 拿大即, 河",
+      5 => "個名, 拿大即, 河, 條聖, 係英國, 拿, 住羅倫",
+      6 => "係英國, 拿, 住羅倫, 加, 大湖, 咗做, 個",
+      7 => "大湖, 咗做, 個, 條聖法話, cane, sole, togatto",
+      8 => "togatto, luna, torriere architettoniche, sostenibilità, tonquinamento, cammello, acquario",
+      9 => "di escrementi di usignolo, tonquinamento atmosferico, acquario, 拿, walter nudo, mare, tonquinamento"
+    }
+    me1 = MediaElement.find 1
+    me1.tags = tag_map[0]
+    assert_obj_saved me1
+    me2 = MediaElement.find 2
+    me2.tags = tag_map[1]
+    assert_obj_saved me2
+    me3 = MediaElement.find 3
+    me3.tags = tag_map[2]
+    assert_obj_saved me3
+    me4 = MediaElement.find 4
+    me4.tags = tag_map[3]
+    assert_obj_saved me4
+    me5 = MediaElement.find 5
+    me5.tags = tag_map[4]
+    assert_obj_saved me5
+    me6 = MediaElement.find 6
+    me6.tags = tag_map[5]
+    assert_obj_saved me6
+    @el1 = MediaElement.new :description => 'desc1', :title => 'titl1'
+    @el1.user_id = 1
+    @el1.sti_type = 'Video'
+    @el1.tags = tag_map[8]
+    @el1.media = {:mp4 => Rails.root.join("test/samples/one.mp4").to_s, :webm => Rails.root.join("test/samples/one.webm").to_s, :filename => "video_test"}
+    assert_obj_saved @el1
+    @el2 = MediaElement.new :description => 'desc2', :title => 'titl2'
+    @el2.user_id = 1
+    @el2.sti_type = 'Video'
+    @el2.tags = tag_map[9]
+    @el2.media = {:mp4 => Rails.root.join("test/samples/one.mp4").to_s, :webm => Rails.root.join("test/samples/one.webm").to_s, :filename => "video_test"}
+    assert_obj_saved @el2
+    @el3 = MediaElement.new :description => 'desc3', :title => 'titl3'
+    @el3.user_id = 1
+    @el3.sti_type = 'Audio'
+    @el3.tags = tag_map[0]
+    @el3.media = {:mp3 => Rails.root.join("test/samples/one.mp3").to_s, :ogg => Rails.root.join("test/samples/one.ogg").to_s, :filename => "audio_test"}
+    assert_obj_saved @el3
+    @el4 = MediaElement.new :description => 'desc4', :title => 'titl4'
+    @el4.user_id = 1
+    @el4.sti_type = 'Audio'
+    @el4.tags = tag_map[6]
+    @el4.media = {:mp3 => Rails.root.join("test/samples/one.mp3").to_s, :ogg => Rails.root.join("test/samples/one.ogg").to_s, :filename => "audio_test"}
+    assert_obj_saved @el4
+    @el5 = MediaElement.new :description => 'desc5', :title => 'titl5'
+    @el5.user_id = 1
+    @el5.sti_type = 'Image'
+    @el5.tags = tag_map[1]
+    @el5.media = File.open(Rails.root.join("test/samples/one.jpg"))
+    assert_obj_saved @el5
+    @el6 = MediaElement.new :description => 'desc6', :title => 'titl6'
+    @el6.user_id = 1
+    @el6.sti_type = 'Image'
+    @el6.tags = tag_map[7]
+    @el6.media = File.open(Rails.root.join("test/samples/one.jpg"))
+    assert_obj_saved @el6
+    @el7 = MediaElement.new :description => 'desc7', :title => 'titl7'
+    @el7.user_id = 1
+    @el7.sti_type = 'Image'
+    @el7.tags = tag_map[2]
+    @el7.media = File.open(Rails.root.join("test/samples/one.jpg"))
+    assert_obj_saved @el7
+    le1 = Lesson.find 1
+    le1.tags = tag_map[3]
+    assert_obj_saved le1
+    le2 = Lesson.find 2
+    le2.tags = tag_map[4]
+    assert_obj_saved le2
+    @les1 = @user1.create_lesson('title1', 'desc1', 1, tag_map[8])
+    @les2 = @user1.create_lesson('title2', 'desc2', 2, tag_map[9])
+    @les3 = @user1.create_lesson('title3', 'desc3', 3, tag_map[0])
+    @les4 = @user1.create_lesson('title4', 'desc4', 4, tag_map[1])
+    @les5 = @user1.create_lesson('title5', 'desc5', 5, tag_map[2])
+    @les6 = @user1.create_lesson('title6', 'desc6', 6, tag_map[3])
+    @les7 = @user1.create_lesson('title7', 'desc7', 1, tag_map[5])
+    @les8 = @user1.create_lesson('title8', 'desc8', 2, tag_map[6])
+    @les9 = @user1.create_lesson('title9', 'desc9', 3, tag_map[7])
+    assert_equal 11, Lesson.count, "Error, a lesson was not saved -- {les1 => #{@les1.inspect}, les2 => #{@les2.inspect}, les3 => #{@les3.inspect}, les4 => #{@les4.inspect}, les5 => #{@les5.inspect}, les6 => #{@les6.inspect}, les7 => #{@les7.inspect}, les8 => #{@les8.inspect}, les9 => #{@les9.inspect},}"
+    assert @les1.publish
+    assert @les2.publish
+    assert @les3.publish
+    assert @les4.publish
+    assert @les5.publish
+    assert @les6.publish
+    # I SET UPDATED AT
+    MediaElement.where(:id => 1).update_all(:updated_at => '2012-01-01 20:00:00')
+    MediaElement.where(:id => 2).update_all(:updated_at => '2012-01-01 19:59:59')
+    MediaElement.where(:id => 3).update_all(:updated_at => '2012-01-01 19:59:58')
+    MediaElement.where(:id => 4).update_all(:updated_at => '2012-01-01 19:59:57')
+    MediaElement.where(:id => 5).update_all(:updated_at => '2012-01-01 19:59:56')
+    MediaElement.where(:id => 6).update_all(:updated_at => '2012-01-01 19:59:55')
+    MediaElement.where(:id => @el1.id).update_all(:updated_at => '2011-10-01 20:00:00', :is_public => true, :publication_date => '2012-01-01 10:00:00')
+    MediaElement.where(:id => @el2.id).update_all(:updated_at => '2011-10-01 19:59:59', :is_public => true, :publication_date => '2012-01-01 10:00:00')
+    MediaElement.where(:id => @el3.id).update_all(:updated_at => '2011-10-01 19:59:58', :is_public => true, :publication_date => '2012-01-01 10:00:00')
+    MediaElement.where(:id => @el4.id).update_all(:updated_at => '2011-10-01 19:59:57')
+    MediaElement.where(:id => @el5.id).update_all(:updated_at => '2011-10-01 19:59:56', :is_public => true, :publication_date => '2012-01-01 10:00:00')
+    MediaElement.where(:id => @el6.id).update_all(:updated_at => '2011-10-01 19:59:55')
+    MediaElement.where(:id => @el7.id).update_all(:updated_at => '2011-10-01 19:59:54', :is_public => true, :publication_date => '2012-01-01 10:00:00')
+    date_now = '2011-01-01 20:00:00'.to_time
+    Lesson.all.each do |l|
+      Lesson.where(:id => l.id).update_all(:updated_at => date_now)
+      date_now -= 1
+    end
+  end
+  
+end
