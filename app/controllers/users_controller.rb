7f7c94ee083c718c9fe57c37db2a795c67401bab
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-03-05 18:20:20 +0100

    piccole correzioni

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -98,9 +98,8 @@ class UsersController < ApplicationController
 
   def statistics
     Statistics.user = current_user
-
-    @my_created_lessons  = Statistics.my_created_lessons.count
-    @my_created_elements = Statistics.my_created_elements.count
+    @my_created_lessons  = Statistics.my_created_lessons
+    @my_created_elements = Statistics.my_created_elements
     @my_copied_lessons   = Statistics.my_copied_lessons
     @my_liked_lessons    = Statistics.my_liked_lessons(3)
     @all_liked_lessons   = Statistics.all_liked_lessons(3)
@@ -108,7 +107,7 @@ class UsersController < ApplicationController
     @all_shared_elements = Statistics.all_shared_elements
     @all_shared_lessons  = Statistics.all_shared_lessons
     @all_users           = Statistics.all_users
-    @all_users_like      = Statistics.all_users_like
+    @all_users_like      = Statistics.all_users_like(3)
     @all_subjects_chart  = Statistics.all_subjects_chart[0].split(',')
     @all_subjects_desc   = Statistics.all_subjects_chart[1].split(',')
   end
