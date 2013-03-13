a7befc9cba1a8b3ea4e304f1723af71ba091ecf0
Maurizio De Santis <m.desantis@morganspa.com>
2013-03-13 16:14:13 +0100

    correzione in _d

--- config/initializers/_d.rb
+++ config/initializers/_d.rb
@@ -1,13 +1,18 @@
 # METODO DI DEBUG
 case Rails.env
 when 'development', 'test'
-  require 'colorize'
-  def _d(*args)
-    puts "#{caller.first}: #{args.map(&:inspect).join(', ')}".yellow
-  end
-  def _d!(*args)
-    _d *args
-    raise '_d!'
+  begin
+    require 'colorize'
+    def _d(*args)
+      puts "#{caller.first}: #{args.map(&:inspect).join(', ')}".yellow
+    end
+    def _d!(*args)
+      _d *args
+      raise '_d!'
+    end
+  rescue LoadError
+    def _d(*_);  end
+    def _d!(*_); end
   end
 else
   # lo dichiaro anche in produzione sia mai che mi scappa di lasciarlo nel codice
