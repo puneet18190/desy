4b8ee8ca7bfd7e8dcad2f6c95b84bf072497b7c7
Maurizio De Santis <m.desantis@morganspa.com>
2013-03-15 12:05:21 +0100

    correzioni EnhancedThread

--- lib/autoload/enhanced_thread.rb
+++ lib/autoload/enhanced_thread.rb
@@ -2,14 +2,14 @@ class EnhancedThread < Thread
 
   DATABASE_POOL = Rails.configuration.database_configuration[Rails.env]['pool']
 
-  def self.join(*procs)
-    procs.each_slice(DATABASE_POOL-1) { |slice| slice.map { |s| new &s }.each(&:join) }
+  def self.join(close_connection_before_execution = false, *procs)
+    procs.each_slice(DATABASE_POOL-1) { |slice| slice.map { |s| new(close_connection_before_execution, &s) }.each(&:join) }
     nil
   end
 
-  def initialize(close_connection = true, &block)
+  def initialize(close_connection_before_execution = false, &block)
     super(
-      if close_connection
+      if close_connection_before_execution
         proc {
           begin
             ActiveRecord::Base.connection.close
@@ -18,7 +18,16 @@ class EnhancedThread < Thread
           block.call
         }
       else
-        block
+        proc {
+          begin
+            block.call
+          ensure
+            begin
+              ActiveRecord::Base.connection.close
+            rescue ActiveRecord::ConnectionTimeoutError
+            end
+          end
+        }
       end
     )
 
--- lib/media/thread.rb
+++ lib/media/thread.rb
@@ -3,5 +3,8 @@ require 'enhanced_thread'
 
 module Media
   class Thread < EnhancedThread
+    def self.join(*procs)
+      super(true, *procs)
+    end
   end
 end
