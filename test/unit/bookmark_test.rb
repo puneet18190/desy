46ca279c4a58c5b702e15afc88f3cec4e9439362
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-03-20 18:01:06 +0100

    tests

--- test/unit/bookmark_test.rb
+++ test/unit/bookmark_test.rb
@@ -28,10 +28,10 @@ class BookmarkTest < ActiveSupport::TestCase
   end
   
   test 'types' do
-    assert_invalid @bookmark, :user_id, 'rt', 1, /is not a number/
-    assert_invalid @bookmark, :user_id, 9.9, 1, /must be an integer/
-    assert_invalid @bookmark, :bookmarkable_id, -8, @lesson.id, /must be greater than 0/
-    assert_invalid @bookmark, :bookmarkable_type, 'Lessen', 'Lesson', /is not included in the list/
+    assert_invalid @bookmark, :user_id, 'rt', 1, :not_a_number
+    assert_invalid @bookmark, :user_id, 9.9, 1, :not_an_integer
+    assert_invalid @bookmark, :bookmarkable_id, -8, @lesson.id, :greater_than
+    assert_invalid @bookmark, :bookmarkable_type, 'Lessen', 'Lesson', :inclusion
     assert_obj_saved @bookmark
   end
   
@@ -41,19 +41,19 @@ class BookmarkTest < ActiveSupport::TestCase
   end
   
   test 'uniqueness' do
-    assert_invalid @bookmark, :bookmarkable_id, 2, @lesson.id, /has already been taken/
+    assert_invalid @bookmark, :bookmarkable_id, 2, @lesson.id, :taken
     @bookmark.bookmarkable_type = 'MediaElement'
     @bookmark.user_id = 2
-    assert_invalid @bookmark, :bookmarkable_id, 4, 6, /has already been taken/
+    assert_invalid @bookmark, :bookmarkable_id, 4, 6, :taken
     assert_obj_saved @bookmark
   end
   
   test 'associations' do
-    assert_invalid @bookmark, :user_id, 1000, 1, /doesn't exist/
-    assert_invalid @bookmark, :bookmarkable_id, 1000, @lesson.id, /lesson doesn't exist/
+    assert_invalid @bookmark, :user_id, 1000, 1, :doesnt_exist
+    assert_invalid @bookmark, :bookmarkable_id, 1000, @lesson.id, :lesson_doesnt_exist
     assert_obj_saved @bookmark
     @bookmark = Bookmark.find 2
-    assert_invalid @bookmark, :bookmarkable_id, 1000, 4, /media element doesn't exist/
+    assert_invalid @bookmark, :bookmarkable_id, 1000, 4, :media_element_doesnt_exist
     assert_obj_saved @bookmark
   end
   
@@ -63,43 +63,39 @@ class BookmarkTest < ActiveSupport::TestCase
     assert !@bookmark.save, "Bookmark erroneously saved - #{@bookmark.inspect}"
     assert_equal 2, @bookmark.errors.messages.length, "A field which wasn't supposed to be affected returned error - #{@bookmark.errors.inspect}"
     assert_equal 2, @bookmark.errors.size
-    assert_match /can't be changed/, @bookmark.errors.messages[:user_id].first
+    assert @bookmark.errors.added? :user_id, :cant_be_changed
     @bookmark.user_id = 1
     assert @bookmark.valid?, "Bookmark not valid: #{@bookmark.errors.inspect}"
     @bookmark.bookmarkable_id = 2
     assert !@bookmark.save, "Bookmark erroneously saved - #{@bookmark.inspect}"
     assert_equal 1, @bookmark.errors.messages.length, "A field which wasn't supposed to be affected returned error - #{@bookmark.errors.inspect}"
-    my_errors = ''
-    @bookmark.errors.messages[:bookmarkable_id].each do |eee|
-      my_errors = "#{my_errors}#{eee}"
-    end
-    assert_match /can't be changed/, my_errors
+    assert @bookmark.errors.added? :bookmarkable_id, :cant_be_changed
     @bookmark.bookmarkable_id = @lesson.id
     assert @bookmark.valid?, "Bookmark not valid: #{@bookmark.errors.inspect}"
     @bookmark.bookmarkable_type = 'MediaElement'
     assert !@bookmark.save, "Bookmark erroneously saved - #{@bookmark.inspect}"
     assert_equal 2, @bookmark.errors.messages.length, "A field which wasn't supposed to be affected returned error - #{@bookmark.errors.inspect}"
     assert_equal 2, @bookmark.errors.size
-    assert_match /can't be changed/, @bookmark.errors.messages[:bookmarkable_type].first
+    assert @bookmark.errors.added? :bookmarkable_type, :cant_be_changed
     @bookmark.bookmarkable_type = 'Lesson'
     assert @bookmark.valid?, "Bookmark not valid: #{@bookmark.errors.inspect}"
     assert_obj_saved @bookmark
   end
   
   test 'availability' do
-    assert_invalid @bookmark, :bookmarkable_id, 1, @lesson.id, /lesson not available for bookmarks/
+    assert_invalid @bookmark, :bookmarkable_id, 1, @lesson.id, :lesson_not_available_for_bookmarks
     @lesson.is_public = false
     assert_obj_saved @lesson
     assert !@bookmark.save, "Bookmark erroneously saved - #{@bookmark.inspect}"
     assert_equal 1, @bookmark.errors.messages.length, "A field which wasn't supposed to be affected returned error - #{@bookmark.errors.inspect}"
     assert_equal 1, @bookmark.errors.size
-    assert_match /lesson not available for bookmarks/, @bookmark.errors.messages[:bookmarkable_id].first
+    assert @bookmark.errors.added? :bookmarkable_id, :lesson_not_available_for_bookmarks
     @lesson.is_public = true
     assert_obj_saved @lesson
     assert @bookmark.valid?, "Bookmark not valid: #{@bookmark.errors.inspect}"
     @bookmark.bookmarkable_type = 'MediaElement'
-    assert_invalid @bookmark, :bookmarkable_id, 1, 2, /media element not available for bookmarks/
-    assert_invalid @bookmark, :bookmarkable_id, 3, 6, /media element not available for bookmarks/
+    assert_invalid @bookmark, :bookmarkable_id, 1, 2, :media_element_not_available_for_bookmarks
+    assert_invalid @bookmark, :bookmarkable_id, 3, 6, :media_element_not_available_for_bookmarks
     assert_obj_saved @bookmark
   end
   
