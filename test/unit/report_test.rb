73099ba82a3757409dfbbb26c895b42188773036
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-03-21 09:31:20 +0100

    tests ancora

--- test/unit/report_test.rb
+++ test/unit/report_test.rb
@@ -23,17 +23,17 @@ class ReportTest < ActiveSupport::TestCase
   end
   
   test 'types' do
-    assert_invalid @report, :user_id, 'ehi', 1, /is not a number/
-    assert_invalid @report, :reportable_id, 5.66, 1, /must be an integer/
-    assert_invalid @report, :user_id, -4, 1, /must be greater than 0/
-    assert_invalid @report, :reportable_type, 'Lazie', 'Lesson', /is not included in the list/
+    assert_invalid @report, :user_id, 'ehi', 1, :not_a_number
+    assert_invalid @report, :reportable_id, 5.66, 1, :not_an_integer
+    assert_invalid @report, :user_id, -4, 1, :greater_than
+    assert_invalid @report, :reportable_type, 'Lazie', 'Lesson', :inclusion
     assert_obj_saved @report
   end
   
   test 'uniqueness' do
-    assert_invalid @report, :reportable_id, 2, 1, /has already been taken/
+    assert_invalid @report, :reportable_id, 2, 1, :taken
     @report.reportable_type = 'MediaElement'
-    assert_invalid @report, :reportable_id, 1, 5, /has already been taken/
+    assert_invalid @report, :reportable_id, 1, 5, :taken
     # I prove that it is possible to report items not public and mine
     assert_equal 'MediaElement', @report.reportable_type
     my_reportable = MediaElement.find(@report.reportable_id)
@@ -43,10 +43,10 @@ class ReportTest < ActiveSupport::TestCase
   end
   
   test 'associations' do
-    assert_invalid @report, :user_id, 1000, 1, /doesn't exist/
-    assert_invalid @report, :reportable_id, 1000, 1, /lesson doesn't exist/
+    assert_invalid @report, :user_id, 1000, 1, :doesnt_exist
+    assert_invalid @report, :reportable_id, 1000, 1, :lesson_doesnt_exist
     @report.reportable_type = 'MediaElement'
-    assert_invalid @report, :reportable_id, 1000, 2, /media element doesn't exist/
+    assert_invalid @report, :reportable_id, 1000, 2, :media_element_doesnt_exist
     assert_obj_saved @report
   end
   
@@ -57,11 +57,11 @@ class ReportTest < ActiveSupport::TestCase
   
   test 'impossible_changes' do
     assert_obj_saved @report
-    assert_invalid @report, :user_id, 2, 1, /can't be changed/
-    assert_invalid @report, :reportable_id, 2, 1, /can't be changed/
+    assert_invalid @report, :user_id, 2, 1, :cant_be_changed
+    assert_invalid @report, :reportable_id, 2, 1, :cant_be_changed
     Report.where(:reportable_type => 'MediaElement', :reportable_id => 1, :user_id => 1).first.destroy
-    assert_invalid @report, :reportable_type, 'MediaElement', 'Lesson', /can't be changed/
-    assert_invalid @report, :comment, "Non ho d'accordo con il contenuto", "Non sono d'accordo con il contenuto", /can't be changed/
+    assert_invalid @report, :reportable_type, 'MediaElement', 'Lesson', :cant_be_changed
+    assert_invalid @report, :comment, "Non ho d'accordo con il contenuto", "Non sono d'accordo con il contenuto", :cant_be_changed
     assert_obj_saved @report
   end
   
