1c3e40266a8613b28711d4f175444ed749760bc0
Andrea D'Ippolito <adedip@gmail.com>
2013-03-26 17:41:33 +0100

    js documentation start

---
+++ doc/js/api.js
@@ -0,0 +1,28 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "bindLoader",
+        "closeGenericVideoComponentCutter",
+        "hideLoader",
+        "initializeVideoEditor",
+        "showLoader",
+        "unbindLoader"
+    ],
+    "modules": [
+        "AjaxLoader",
+        "VideoEditor"
+    ],
+    "allModules": [
+        {
+            "displayName": "AjaxLoader",
+            "name": "AjaxLoader",
+            "description": "Show a loading image while page is loading, \nit handles ajax calls too."
+        },
+        {
+            "displayName": "VideoEditor",
+            "name": "VideoEditor",
+            "description": "Provides the base Widget class..."
+        }
+    ]
+} };
+});
