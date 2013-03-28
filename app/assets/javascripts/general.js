f28b16d6eb9f5b7034122b1d8aae080aa27b4ba7
Andrea D'Ippolito <adedip@gmail.com>
2013-03-28 10:09:34 +0100

    general docjs

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,3 +1,19 @@
+/**
+* Generic shared javascript functions
+* 
+* @module General
+*/
+
+/**
+* Add new parameters to an url
+* 
+* @method updateURLParameter
+* @for updateURLParameter
+* @param url {String} starting url
+* @param param {String} new param
+* @param paramVal {String} new param value
+* @return {String} updated url
+*/
 function updateURLParameter(url, param, paramVal) {
   var newAdditionalURL = '';
   var tempArray = url.split('?');
@@ -17,6 +33,15 @@ function updateURLParameter(url, param, paramVal) {
   return baseURL + '?' + newAdditionalURL + rows_txt;
 }
 
+/**
+* Remove a param from url
+* 
+* @method removeURLParameter
+* @for removeURLParameter
+* @param url {String} starting url
+* @param param {String} param to remove
+* @return {String} update url
+*/
 function removeURLParameter(url, param) {
   var newAdditionalURL = '';
   var tempArray = url.split('?');
@@ -35,6 +60,16 @@ function removeURLParameter(url, param) {
   return baseURL + '?' + newAdditionalURL;
 }
 
+/**
+* Center a div into a given container
+* 
+* @method centerThisInContainer
+* @for centerThisInContainer
+* @param div {String} div selector to center, class or id
+* @param container {String} container selector to center div into, class or id
+* @example
+      centerThisInContainer('._saved','#image_wrapper');
+*/
 function centerThisInContainer(div,container) {
   var contH = $(container).height();
   var contW = $(container).width();
@@ -43,6 +78,15 @@ function centerThisInContainer(div,container) {
   centerDiv.css('left', (contW/2-centerDiv.width()/2)+$(container).position().left);
 }
 
+/**
+* Center a div into current window
+* 
+* @method centerThis
+* @for centerThis
+* @param div {String} div selector to center, class or id
+* @example
+      centerThis("._slide_error");
+*/
 function centerThis(div) {
   var winH = $(window).height();
   var winW = $(window).width();
