3d5a66e4b32788fbc9867af5c85e8097c5b6b156
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-04-02 16:11:51 +0200

    avanti con autoload

--- lib/autoload/lessons_media_elements_shared.rb
+++ lib/autoload/lessons_media_elements_shared.rb
@@ -1,13 +1,17 @@
+# List of common translation methods that call I18n
 module LessonsMediaElementsShared
   
+  # Translation of the status of a lesson or of an element (see Statuses)
   def status(s)
     I18n.t("statuses.#{table_name}.#{s}")
   end
   
+  # Translation of the filter of a lesson or of an element (see Filters)
   def filter(f)
     I18n.t("filters.#{f}")
   end
   
+  # Translation of the search order of a lesson or of an element (see SearchOrders)
   def search_order(so)
     I18n.t("orders.#{so}")
   end
