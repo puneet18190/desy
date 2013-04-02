fe53ede6a68162161c22f3643cc035ea9f7272aa
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-04-02 15:57:28 +0200

    autoload 1

--- lib/autoload/buttons.rb
+++ lib/autoload/buttons.rb
@@ -1,18 +1,45 @@
 module Buttons
   
+  # Keyword for the button that opens the preview
   PREVIEW = 'preview'
+  
+  # Keyword for the button that opens the editor
   EDIT = 'edit'
+  
+  # Keyword for the button that publishes a private lesson
   PUBLISH = 'publish'
+  
+  # Keyword for the button that turns back a public lesson into private
   UNPUBLISH = 'unpublish'
+  
+  # Keyword for the button that linnks a lesson or an element
   ADD = 'add'
+  
+  # Keyword for the button that removes the link of a lesson or of an element
   REMOVE = 'remove'
+  
+  # Keyword for the button that destroys a lesson or an element
   DESTROY = 'destroy'
+  
+  # Keyword for the button that records a "I like you" on a lesson
   LIKE = 'like'
+  
+  # Keyword for the button that removes a "I like you" from a lesson
   DISLIKE = 'dislike'
+  
+  # Keyword for the button that sends a report of inappropriate content about a lesson or an element
   REPORT = 'report'
+  
+  # Keyword for the button that adds a lesson to your Virtual Classroom
   ADD_VIRTUAL_CLASSROOM = 'add_virtual_classroom'
+  
+  # Keyword for the button that removes a lesson from your Virtual Classroom
   REMOVE_VIRTUAL_CLASSROOM = 'remove_virtual_classroom'
+  
+  # Keyword for the button that creates a copy of a lesson
   COPY = 'copy'
+  
+  # Keyword for the button that opens the form to modify general information about an element
   CHANGE_INFO = 'change_info'
   
 end
