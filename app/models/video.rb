12ecc263fec05b0ffe343c7621cf15de1bb6894e
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-04-02 12:39:42 +0200

    video iniziato

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,30 +2,104 @@ require 'media/video/uploader'
 require 'media/video/editing/parameters'
 require 'media/shared'
 
+# == Description
+#
+# This class inherits from MediaElement, and contains the specific methods needed for media elements of type +video+. For methods shared by elements of type +audio+ and +video+, see Media::Shared.
+# 
 class Video < MediaElement
+  
+  # Instance of specific uploader for a video
   UPLOADER = Media::Video::Uploader
+  
+  # List of accepted extensions
   EXTENSION_WHITE_LIST = UPLOADER::EXTENSION_WHITE_LIST
+  
+  # Path for restoring the video editor cache
   CACHE_RESTORE_PATH = '/videos/cache/restore'
   
   include Media::Shared
   extend  Media::Video::Editing::Parameters
   
+  # === Description
+  #
+  # Returns the lower integer approximation of the minimum between +webm_duration+ and +mp4_duration+. This is necessary to insert in the html players an integer duration in seconds that can be used without risks.
+  #
+  # === Returns
+  #
+  # An integer.
+  #
+  # === Usage
+  #
+  #   <div class="videoPlayer _instance_of_player" data-media-type="video" data-initialized="false" data-duration="<%= video.min_duration %>">
+  #
   def min_duration
     [mp4_duration, webm_duration].map(&:to_i).min
   end
   
+  # === Description
+  #
+  # Returns the url for the video in format +mp4+
+  #
+  # === Returns
+  #
+  # An url.
+  #
+  # === Usage
+  #
+  #   <video>
+  #     <source src="<%= video.mp4_url %>" type="video/mp4">
+  #   </video>
+  #
   def mp4_url
     media.try(:url, :mp4) if converted
   end
   
+  # === Description
+  #
+  # Returns the url for the video in format +webm+
+  #
+  # === Returns
+  #
+  # An url.
+  #
+  # === Usage
+  #
+  #   <video>
+  #     <source src="<%= video.webm_url %>" type="video/webm">
+  #   </video>
+  #
   def webm_url
     media.try(:url, :webm) if converted
   end
   
+  # === Description
+  #
+  # Returns the url of the image used anywhere it's necessary the cover of the video with original proportions (it's the middle frame of the video, in 960 x 540 pixels).
+  #
+  # === Returns
+  #
+  # An url.
+  #
+  # === Usage
+  #
+  #   <%= image_tag video.cover_url %>
+  #
   def cover_url
     media.try(:url, :cover) if converted
   end
   
+  # === Description
+  #
+  # Returns the url of the thumb image used in the section "elements" (the middle frame of the video resized to 200 x 200 pixels).
+  #
+  # === Returns
+  #
+  # An url.
+  #
+  # === Usage
+  #
+  #   <%= image_tag video.thumb_url %>
+  #
   def thumb_url
     media.try(:url, :thumb) if converted
   end
