7e0fd70d175627372bbf7ccaf9d4c487e5476496
Maurizio De Santis <m.desantis@morganspa.com>
2013-04-03 16:10:48 +0200

    Media::Audio::Uploader : adding audio loading valid extensions

--- lib/media/audio/uploader.rb
+++ lib/media/audio/uploader.rb
@@ -16,7 +16,7 @@ module Media
 
       PUBLIC_RELATIVE_FOLDER        = env_relative_path File.join(PUBLIC_RELATIVE_MEDIA_ELEMENTS_FOLDER, 'audios')
       FOLDER                        = File.join RAILS_PUBLIC_FOLDER, PUBLIC_RELATIVE_FOLDER
-      EXTENSION_WHITE_LIST          = %w(mp3 ogg flac aiff wav wma aac)
+      EXTENSION_WHITE_LIST          = %w(mp3 ogg oga flac aiff wav wma aac m4a)
       EXTENSION_WHITE_LIST_WITH_DOT = EXTENSION_WHITE_LIST.map{ |ext| ".#{ext}" }
       MIN_DURATION                  = 1
       DURATION_THRESHOLD            = CONFIG.duration_threshold
