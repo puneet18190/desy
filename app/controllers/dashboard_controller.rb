dcc21a05ecbc8cb2de36fd30e6fcf367de4a8b4a
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-04-03 10:03:53 +0200

    oka

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,29 @@
+# == Description
+#
+# Controller for the dashboard
+#
+# == Models used
+#
+# * Lesson
+# * MediaElement
+#
 class DashboardController < ApplicationController
   
   before_filter :initialize_layout, :initialize_pagination
   
+  # === Description
+  #
+  # Extracts suggested lessons and elements (see User#suggested_lessons, User#suggested_media_elements)
+  #
+  # === Mode
+  #
+  # Html + Ajax
+  #
+  # === Specific filters
+  #
+  # * DashboardController#initialize_pagination
+  # * ApplicationController#initialize_layout
+  #
   def index
     @lessons = current_user.suggested_lessons(@lessons_for_page * @lesson_pages)
     @lessons_emptied = Lesson.dashboard_emptied? current_user.id
