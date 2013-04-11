41eb45665909e762f228c69b9339344ed23aa4d3
Adriano Di Lauro <a.dilauro@morganspa.com>
2013-04-11 17:59:19 +0200

    okOk

--- app/assets/javascripts/admin/functions.js
+++ app/assets/javascripts/admin/functions.js
@@ -1,5 +1,5 @@
 /**
-Functions used in the Administration section: only for this section, it's generated a separate file which doesn't merge with the regular one. The only external module loaded is {{#crossLinkModule "ajax-loader"}}{{/crossLinkModule}}.
+Functions used in the Administration section: only for this section, <b>it's generated a separate file</b> which doesn't merge with the regular one. The only external module loaded is {{#crossLinkModule "ajax-loader"}}{{/crossLinkModule}}.
 @module administration
 **/
 
--- doc/js/api.js
+++ doc/js/api.js
@@ -118,7 +118,7 @@ YUI.add("yuidoc-meta", function(Y) {
         {
             "displayName": "administration",
             "name": "administration",
-            "description": "Functions used in the Administration section: only for this section, it's generated a separate file which doesn't merge with the regular one. The only external module loaded is {{#crossLinkModule \"ajax-loader\"}}{{/crossLinkModule}}."
+            "description": "Functions used in the Administration section: only for this section, <b>it's generated a separate file</b> which doesn't merge with the regular one. The only external module loaded is {{#crossLinkModule \"ajax-loader\"}}{{/crossLinkModule}}."
         },
         {
             "displayName": "ajax-loader",
