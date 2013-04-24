a3b6e726a8a79c1f729ae19883824c3e8824bb7e
Maurizio De Santis <m.desantis@morganspa.com>
2013-04-24 18:02:28 +0200

    docs

--- lib/media/uploader.rb
+++ lib/media/uploader.rb
@@ -45,11 +45,17 @@ module Media
       media_elements_folder_size > MAXIMUM_MEDIA_ELEMENTS_FOLDER_SIZE
     end
 
-    # Called by the relative model attribute setter and getter; it currently supports instances of the following classes:
-    # * +String+
-    # * +File+
-    # * +ActionDispatch::Http::UploadedFile+
-    # * +Hash+
+    # Called by the relative model attribute setter and getter
+    #
+    # === Args
+    #
+    # * *model*: instance to which the media supplied will be associated
+    # * *column*: column of the record table which will contain the media name
+    # * *value*: value of the media; it supports instances of the following classes:
+    #   * +String+
+    #   * +File+
+    #   * +ActionDispatch::Http::UploadedFile+
+    #   * +Hash+
     def initialize(model, column, value)
       @model, @column, @value = model, column, value
 
@@ -72,44 +78,54 @@ module Media
       end
     end
 
+    # Filename of the media relative to the supplied format
     def filename(format)
       "#{filename_without_extension}.#{format}"
     end
 
+    # Filename without the extension (related to the format)
     def filename_without_extension
       (@filename_without_extension || original_filename_without_extension).to_s
     end
 
+    # Returns a new filename adding a token (provided by the model instance), adding the extension if present
     def process(filename, extension = nil)
       filename = "#{filename.parameterize}_#{model.filename_token}"
       filename << ".#{extension}" if extension
       filename
     end
 
+    # Original filename after being processed by Media::Uploader.process
     def processed_original_filename
       process(original_filename_without_extension, original_filename_extension)
     end
 
+    # Original filename after being processed by Media::Uploader.process without the extension
     def processed_original_filename_without_extension
       process(original_filename_without_extension)
     end
 
+    # Original filename without the extension
     def original_filename_without_extension
       @original_filename_without_extension || File.basename(original_filename, original_filename_extension)
     end
 
+    # Original filename
     def original_filename
       File.basename(@original_filename)
     end
 
+    # Original filename extension
     def original_filename_extension
       File.extname(@original_filename)
     end
 
+    # Returns +false+ (a Media::Uploader instance is never +blank?+)
     def blank?
       false
     end
 
+    # Start the upload or the copy processing, depending on <tt>@value</tt>
     def upload_or_copy
       raise Error.new('model_id cannot be blank', model: @model, column: @column, value: @value) if model_id.blank?
 
@@ -122,35 +138,43 @@ module Media
       true
     end
 
+    # Path where the uploaded/copied file will be written without the extension
     def output_path_without_extension
       File.join output_folder, processed_original_filename_without_extension
     end
 
+    # Path where the uploaded/copied file will be written (related to the format)
     def output_path(format)
       "#{output_path_without_extension}.#{format}"
     end
 
+    # Folder of the output path
     def folder
       File.join self.class::FOLDER, model_id.to_s
     end
     alias output_folder folder
 
+    # Folder of the output path relative to the Rails static resources folder (in order to be used by URLs)
     def public_relative_folder
       File.join '/', self.class::PUBLIC_RELATIVE_FOLDER, model_id.to_s
     end
 
+    # id of the model instance
     def model_id
       model.id
     end
 
+    # Whether the value of the attribute of the instance is changed
     def column_changed?
       model.send(:"#{column}_changed?")
     end
 
+    # Whether the value of the attribute of the instance can be renamed
     def rename?
       model.send(:"rename_#{column}")
     end
 
+    # If the attribute is changed and can be renamed returns the processed filename, otherwise returns Media::Uploader#public_relative_path
     def to_s
       if column_changed? and rename?
         process(@value.to_s)
@@ -160,6 +184,7 @@ module Media
     end
     alias inspect to_s
 
+    # Returns the public relative path related to the supplied format, or without extension if format is blank
     def public_relative_path(format = nil)
       File.join public_relative_folder, (
         case format
@@ -176,6 +201,7 @@ module Media
     end
     alias url public_relative_path
 
+    # Absolute path of the file, related to the supplied +format+ (+format+ required)
     def path(format)
       case format
       when *self.class::FORMATS
@@ -185,16 +211,19 @@ module Media
       end
     end
 
+    # An hash where the keys are the supported formats (version formats included) and the values the absolute paths of the media files
     def paths
       Hash[ (self.class::FORMATS + self.class::VERSION_FORMATS.keys).map{ |f| [f, path(f)] } ]
     end
 
+    # An hash where the keys are the supported formats (version formats excluded) plus a pair key => value where the key is +:filename+ and the value Media::Uploader#filename_without_extension
     def to_hash
       Hash[ self.class::FORMATS.map{ |f| [f, path(f)] } ].merge(filename: filename_without_extension)
     end
 
     private
-    def copy
+    # Executes the copy process
+    def copy # :doc:
       FileUtils.mkdir_p output_folder unless Dir.exists? output_folder
       
       infos = {}
@@ -223,14 +252,17 @@ module Media
       true
     end
 
-    def extract_versions(infos)
+    # Implemented by the descendant classes in order to extract the versions needed (used by Media::Uploader.copy)
+    def extract_versions(infos) # :doc:
     end
 
+    # Executes the copy of the uploaded file to the temporary folder and add related job to the jobs queue
     def upload_copy_and_job(conversion_temp_path)
       FileUtils.cp @original_file.path, conversion_temp_path
       Delayed::Job.enqueue self.class::CONVERSION_CLASS::Job.new(@original_file.path, output_path_without_extension, original_filename, model_id)
     end
 
+    # Executes the upload process
     def upload
       return if model.skip_conversion
 
