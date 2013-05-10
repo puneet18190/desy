75ec8bbc03e28f397a4d5e91e3e068ae06fe60f0
Adriano di Lauro <a.dilauro@morganspa.com>
2013-05-10 09:45:58 +0200

    ci siamo, finita correzione

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,17 +1,17 @@
 module I18n
+  include ERB::Util
   
   class << self
     
     def translate_with_html_escaping(*args)
       if args[-1].is_a?(Hash)
-        args[-1] = Hash[ args[-1].map{ |k,v| [k, ([:locale, :throw, :raise].include?(k) ? v : h(v)) ] } ]
+        args[-1] = Hash[ args[-1].map{ |k,v| [k, ([:locale, :throw, :raise].include?(k) ? v : ERB::Util.h(v)) ] } ]
       end
       translate_without_html_escaping(*args)
     end
     
-    #=> translate -> translate_without_html_escaping
-    #=> translate_with_html_escaping -> translate
     alias_method_chain :translate, :html_escaping
+    alias_method :t, :translate_with_html_escaping
     
   end
   
