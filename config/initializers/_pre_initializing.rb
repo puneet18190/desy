55405a1ceb22fece7255e14bec58387ded91d640
Windows <windows@morganspa.com>
2013-06-19 17:51:39 +0200

    fixings for windows support

--- config/initializers/_pre_initializing.rb
+++ config/initializers/_pre_initializing.rb
@@ -15,3 +15,35 @@ module Arel
     end
   end
 end
+
+# Patch to Facter in order to fix EncodingError on Windows
+require 'facter'
+module Facter::Util::Config
+  def self.windows_data_dir
+    if Dir.const_defined? 'COMMON_APPDATA' then
+      Dir::COMMON_APPDATA.encode('ASCII')
+    else
+      nil
+    end
+  end
+end
+
+WINDOWS = Facter.kernel == 'windows'
+
+if WINDOWS
+  require 'win32/dir'
+  # Make Dir.glob work with Pathname instances
+  class Dir
+    class << self
+      def glob(glob_pattern, flags = 0, &block)
+        glob_pattern = glob_pattern.to_s.tr("\\", "/")
+        old_glob(glob_pattern, flags, &block)
+      end
+
+      def [](glob_pattern)
+        glob_pattern = glob_pattern.to_s.tr("\\", "/")
+        old_ref(glob_pattern)
+      end
+    end
+  end
+end
