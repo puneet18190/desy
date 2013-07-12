055a6541dc8a861bff784fc249654f0ef4a11fe6
Adriano di Lauro <a.dilauro@morganspa.com>
2013-07-12 11:19:25 +0200

    searhc

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -13,6 +13,16 @@ Initializer for documents functionality.
 @for DocumentsDocumentReady
 **/
 function documentsDocumentReady() {
+  documentsDocumentReadyGeneral();
+  documentsDocumentReadySearch();
+}
+
+/**
+Initializer for documents functionality (general javascripts).
+@method documentsDocumentReadyGeneral
+@for DocumentsDocumentReady
+**/
+function documentsDocumentReadyGeneral() {
   $('body').on('mouseover', '._empty_documents', function() {
     $(this).find('._empty_documents_hover').addClass('current');
   });
@@ -21,6 +31,28 @@ function documentsDocumentReady() {
   });
   $('body').on('click', '._empty_documents, #upload_document', function() {
     openNewDocumentLoader();
+  });
+}
+
+/**
+Initializer for documents functionality (search and placeholders javascripts).
+@method documentsDocumentReadySearch
+@for DocumentsDocumentReady
+**/
+function documentsDocumentReadySearch() {
+  $('body').on('click', '#search_documents_submit', function() {
+    if(!$(this).hasClass('current')) {
+      $('#search_documents').submit();
+      $(this).addClass('current');
+    }
+  });
+  $('body').on('focus', '#search_documents ._word_input', function() {
+    if($('#search_documents_placeholder').val() == '') {
+      $('#search_documents_submit').removeClass('current');
+      $(this).val('');
+      $(this).css('color', '#939393');
+      $('#search_documents_placeholder').val('0');
+    }
   });
 }
 
