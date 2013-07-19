a0294f902f6bf0761942981e5aa809a9e97cc39b
Adriano di Lauro <a.dilauro@morganspa.com>
2013-07-19 18:18:49 +0200

    javascript temporaneo

--- app/assets/javascripts/lesson_editor.js
+++ app/assets/javascripts/lesson_editor.js
@@ -441,6 +441,11 @@ function lessonEditorDocumentReadySlideButtons() {
       url: '/lessons/' + $('#info_container').data('lesson-id') + '/slides/' + slide.data('slide-id') + '/delete'
     });
   });
+  $('body').on('click', '._attach_document', function() {
+    
+    alert('sto allegando un documento'); // TODO
+    
+  });
 }
 
 /**
