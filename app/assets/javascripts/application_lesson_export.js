2e12b58da2d9aa589ab3421e29de9c90fc938226
Adriano di Lauro <a.dilauro@morganspa.com>
2013-09-04 16:53:48 +0200

    iniziato application_lesson_export

--- app/assets/javascripts/application_lesson_export.js
+++ app/assets/javascripts/application_lesson_export.js
@@ -16,38 +16,16 @@
 //= require jquery_patches
 //= require jquery_ujs
 //= require jquery.mousewheel
-//= require jquery.jscrollpane
 //= require jquery.fullscreen
 //= require jquery.event.move
 //= require jquery.event.swipe
 //= require jquery.selectbox-0.2.custom
 //= require jquery-ui-1.9.0.custom
-//= require jquery.imgareaselect
 //= require jquery.peity
-//= require jquery-fileupload/basic
-//= require tinymce-jquery
-//= require dots_pagination
 //= require jquery.formparams
-//= require ajax_loader
-//= require audio_editor
-//= require buttons
-//= require dashboard
-//= require dialogs
-//= require documents
-//= require galleries
 //= require general
-//= require image_editor
-//= require lesson_editor
 //= require lesson_viewer
-//= require media_element_editor
-//= require media_element_loader
-//= require notifications
 //= require players
-//= require profile
-//= require search
-//= require tags
-//= require video_editor
-//= require virtual_classroom
 
 
 $(document).ready(function() {
