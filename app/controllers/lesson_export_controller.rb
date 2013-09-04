d7514a83887ca2af12d4cf86c7534e339fafc5fd
Adriano di Lauro <a.dilauro@morganspa.com>
2013-09-04 09:32:54 +0200

    iniziata branch lesson export

--- app/controllers/lesson_export_controller.rb
+++ app/controllers/lesson_export_controller.rb
@@ -3,15 +3,13 @@ require 'export'
 class LessonExportController < ApplicationController
   
   layout 'lesson_export', only: :export
-
+  
   def export
-    initialize_lesson
-
+    initialize_lesson # TODO
     @slides = @lesson.slides.order(:position)
     @cover_img = @slides.first.media_elements_slides.first
-
     archive_path = Export::Lesson::Archive.new(@lesson, render_to_string(action: :index)).public_path
     redirect_to archive_path
   end
-
+  
 end
