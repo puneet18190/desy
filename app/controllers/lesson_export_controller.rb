226571073b779c4dfbb9ab4908d0752f4bb3d1d0
Adriano di Lauro <a.dilauro@morganspa.com>
2013-09-04 12:46:27 +0200

    rifiniture

--- app/controllers/lesson_export_controller.rb
+++ app/controllers/lesson_export_controller.rb
@@ -1,11 +1,34 @@
 require 'export'
 
+# == Description
+#
+# Controller for exporting lessons
+#
+# == Models used
+#
+# * Lesson
+# * Slide
+# * DocumentsSlide
+# * MediaElementsSlide
+#
 class LessonExportController < ApplicationController
   
+  before_filter :initialize_lesson_with_owner_or_public
   layout 'lesson_export', only: :export
   
+  # === Description
+  #
+  # Downloads the lesson offline.
+  #
+  # === Mode
+  #
+  # Html
+  #
+  # === Specific filters
+  #
+  # * ApplicationController#initialize_lesson_with_owner_or_public
+  #
   def export
-    initialize_lesson # TODO
     @slides = @lesson.slides.order(:position)
     @cover_img = @slides.first.media_elements_slides.first
     archive_path = Export::Lesson::Archive.new(@lesson, render_to_string(action: :index)).public_path
