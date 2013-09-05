7e84dbde5fe6dfa0841a6ddd247331130fa7d895
Adriano di Lauro <a.dilauro@morganspa.com>
2013-09-05 17:21:03 +0200

    force_share rimosso

--- app/controllers/lesson_export_controller.rb
+++ app/controllers/lesson_export_controller.rb
@@ -32,7 +32,7 @@ class LessonExportController < ApplicationController
     @slides = @lesson.slides.order(:position)
     @cover_img = @slides.first.media_elements_slides.first
     @with_exit = false
-    @force_share = true
+    @export = true
     archive_url = Export::Lesson::Archive.new(@lesson, render_to_string(action: :index)).url
     redirect_to archive_url
   end
