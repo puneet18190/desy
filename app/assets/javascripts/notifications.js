c1c01590c79db4c6fda14bddee315a95f8b2a9c2
Adriano di Lauro <a.dilauro@morganspa.com>
2013-10-02 16:54:53 +0200

    sostituiti ul e li con div in notifications, e controllato attentamente il risultato

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -259,7 +259,7 @@ function initializeNotifications() {
     }
   });
   $body.on('click', '._single_notification .notification_gatto', function() {
-    var closest_li = $(this).closest('li');
+    var closest_li = $(this).closest('.li_notifications');
     var my_own_id = closest_li.attr('id')
     var my_content = $('#' + my_own_id + ' ._expanded_notification').html();
     var my_expanded = $('#expanded_notification');
