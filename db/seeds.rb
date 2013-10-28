b11673db0f00c13a1f478c941e519509b207c482
Adriano di Lauro <a.dilauro@morganspa.com>
2013-10-28 15:15:54 +0100

    seeds avanti

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,35 +3,38 @@ require 'env_relative_path'
 
 class Seeds
   include EnvRelativePath
-
+  
+  # Directory containing csv
   CSV_DIR     = Rails.root.join("db/seeds/environments/#{Rails.env}/csv")
+  # Options for the csv loading
   CSV_OPTIONS = { headers: true }
-
+  # Public media element folders
   PUBLIC_MEDIA_ELEMENTS_FOLDERS     = [ Media::Video::Uploader, Media::Audio::Uploader, ImageUploader ].map{ |u| u.const_get(:FOLDER) }
+  # Folder where backups of media elements are stored
   OLD_PUBLIC_MEDIA_ELEMENTS_FOLDERS = Hash[ PUBLIC_MEDIA_ELEMENTS_FOLDERS.map{ |f| [ f, "#{f}.old" ] } ]
-  
+  # Normal folder of media elements
   MEDIA_ELEMENTS_FOLDER = Rails.root.join "db/seeds/environments/#{Rails.env}/media_elements"
-  
+  # Public documents folder
   PUBLIC_DOCUMENTS_FOLDER     = DocumentUploader::FOLDER
+  # Folder where backups of documents are stored
   OLD_PUBLIC_DOCUMENTS_FOLDER = "#{DocumentUploader::FOLDER}.old"
-  
+  # Folder of documents
   DOCUMENTS_FOLDER = Rails.root.join "db/seeds/environments/#{Rails.env}/documents"
-
+  # Pepper code
   PEPPER = '3e0e6d5ebaa86768a0a51be98fce6367e44352d31685debf797b9f6ccb7e2dd0f5139170376240945fcfae8222ff640756dd42645336f8b56cdfe634144dfa7d'
-
+  
   # AUDIOS_FOLDER, VIDEOS_FOLDER, IMAGES_FOLDER
   %w(audios videos images).each do |media_folder|
     const_set :"#{media_folder.upcase}_FOLDER", Pathname.new(env_relative_path MEDIA_ELEMENTS_FOLDER, media_folder)
   end
-
-  MODELS = [ Location, SchoolLevel, Subject, User, Document, MediaElement, Lesson, Slide, MediaElementsSlide, Like, Bookmark ]
-
+  
+  # List of models to seed
+  MODELS = [ Location, SchoolLevel, Subject, User, Document, MediaElement, Lesson, Slide, MediaElementsSlide, Like, Bookmark, Purchase ]
+  
   def run
     puts "Applying #{Rails.env} seeds (#{MODELS.map{ |m| humanize_table_name(m.table_name) }.join(', ')})"
-
     backup_old_media_elements_folders
     backup_old_documents_folder
-
     ActiveRecord::Base.transaction do
       MODELS.each do |model|
         @model, @table_name = model, model.table_name
@@ -39,50 +42,42 @@ class Seeds
         send :"#{@table_name}!"
         update_sequence
       end
-
       remove_old_media_elements_folders
       remove_old_documents_folder
     end
-
     replace_pepper
-
     puts 'End.'
   rescue StandardError => e
     restore_old_media_elements_folders
     restore_old_documents_folder
-
     raise e
   end
-
+  
   private
+  
   def replace_pepper
     return if User::Authentication::PEPPER == PEPPER
-
     old_pepper = "#{User::Authentication::PEPPER_PATH}.old"
     pepper = User::Authentication::PEPPER_PATH
-
     warn "The pepper doesn't correspond to the seeds pepper; replacing" 
     warn "Moving #{pepper} to #{old_pepper}"
-
     FileUtils.mv pepper, old_pepper
-
     pepper.open('w'){ |io| io.write PEPPER }
     User::Authentication.const_set :PEPPER, PEPPER
   end
-
+  
   def backup_old_documents_folder
     remove_old_documents_folder
-    
     begin
       FileUtils.mv PUBLIC_DOCUMENTS_FOLDER, OLD_PUBLIC_DOCUMENTS_FOLDER
     rescue Errno::ENOENT
     end
   end
-
+  
   def remove_old_documents_folder
     FileUtils.rm_rf OLD_PUBLIC_DOCUMENTS_FOLDER
   end
-
+  
   def restore_old_documents_folder
     begin
       FileUtils.rm_rf PUBLIC_DOCUMENTS_FOLDER
@@ -90,10 +85,9 @@ class Seeds
     rescue Errno::ENOENT
     end
   end
-
+  
   def backup_old_media_elements_folders
     remove_old_media_elements_folders
-    
     PUBLIC_MEDIA_ELEMENTS_FOLDERS.each_with_index do |f|
       begin
         FileUtils.mv f, OLD_PUBLIC_MEDIA_ELEMENTS_FOLDERS[f]
@@ -101,11 +95,11 @@ class Seeds
       end
     end
   end
-
+  
   def remove_old_media_elements_folders
     OLD_PUBLIC_MEDIA_ELEMENTS_FOLDERS.values.each{ |of| FileUtils.rm_rf of }
   end
-
+  
   def restore_old_media_elements_folders
     OLD_PUBLIC_MEDIA_ELEMENTS_FOLDERS.each do |f, of|
       begin
@@ -115,19 +109,19 @@ class Seeds
       end
     end
   end
-
+  
   def humanize_table_name(table_name = @table_name)
     table_name.tr('_', ' ')
   end
-
+  
   def csv_path(filename = @table_name)
     CSV_DIR.join "#{filename}.csv"
   end
-
+  
   def csv_open(csv_path = csv_path)
     CSV.open(csv_path, CSV_OPTIONS)
   end
-
+  
   def users_subjects(id)
     csv_open(csv_path('users_subjects')).each.select do |row|
       row['user_id'] == id.to_s
@@ -135,7 +129,7 @@ class Seeds
       row['subject_id']
     end
   end
-
+  
   def media(record, row)
     case record
     when Audio
@@ -153,7 +147,7 @@ class Seeds
       File.open Pathname.glob(IMAGES_FOLDER.join(record.id.to_s, Image::EXTENSIONS_GLOB), File::FNM_CASEFOLD).first
     end
   end
-
+  
   def attachment(id)
     f = case id % 6
       when 0 then 'doc1.ppt'
@@ -165,7 +159,7 @@ class Seeds
     end
     File.open DOCUMENTS_FOLDER.join f
   end
-
+  
   def tags(id)
     csv_open(csv_path('taggings')).each.select do |row|
       row['taggable_type'] == @model.to_s && row['taggable_id'] == id.to_s
@@ -173,54 +167,51 @@ class Seeds
       csv_open(csv_path('tags')).each.find{ |tags_row| taggable_row['tag_id'] == tags_row['id'] }['word']
     end.join(',')
   end
-
+  
   def csv_row_to_record(row, model = @model, skip = [])
     model.new do |record|
       row.headers.reject{ |h| skip.include? h }.each { |header| record.send :"#{header}=", row[header] }
     end
   end
-
+  
   def update_sequence
     @model.connection.reset_pk_sequence! @table_name
   end
-
+  
   def progress(i)
     n = i+1
-
     $stdout.print "  saving #{n.ordinalize} of #{@rows_amount} #{humanize_table_name}\r"
     $stdout.flush
-
     return if n != @rows_amount
-
     $stdout.puts
     $stdout.flush
   end
-
+  
   def set_rows_amount
     @rows_amount = csv_open.readlines.count
   end
-
+  
   def locations!
     csv_open.each.each_with_index do |row, i|
       progress(i)
       csv_row_to_record(row).save!
     end
   end
-
+  
   def school_levels!
     csv_open.each.each_with_index do |row, i|
       progress(i)
       csv_row_to_record(row).save!
     end
   end
-
+  
   def subjects!
     csv_open.each.each_with_index do |row, i|
       progress(i)
       csv_row_to_record(row).save!
     end
   end
-
+  
   def users!
     csv_open.each.each_with_index do |row, i|
       progress(i)
@@ -230,7 +221,15 @@ class Seeds
       record.save!
     end
   end
-
+  
+  def purchases!
+    csv_open.each.each_with_index do |row, i|
+      progress(i)
+      record = csv_row_to_record(row)
+      record.save!
+    end
+  end
+  
   def media_elements!
     csv_open.each.each_with_index do |row, i|
       progress(i)
@@ -242,7 +241,7 @@ class Seeds
       record.save!
     end
   end
-
+  
   def lessons!
     csv_open.each.each_with_index do |row, i|
       progress(i)
@@ -254,7 +253,7 @@ class Seeds
       record.save!
     end
   end
-
+  
   def documents!
     csv_open.each.each_with_index do |row, i|
       progress(i)
@@ -263,35 +262,35 @@ class Seeds
       record.save!
     end
   end
-
+  
   def slides!
     csv_open.each.each_with_index do |row, i|
       progress(i)
       csv_row_to_record(row).save!
     end
   end
-
+  
   def media_elements_slides!
     csv_open.each.each_with_index do |row, i|
       progress(i)
       csv_row_to_record(row).save!
     end
   end
-
+  
   def likes!
     csv_open.each.each_with_index do |row, i|
       progress(i)
       csv_row_to_record(row).save!
     end
   end
-
+  
   def bookmarks!
     csv_open.each.each_with_index do |row, i|
       progress(i)
       csv_row_to_record(row).save!
     end
   end
-
+  
 end
 
 Seeds.new.run
