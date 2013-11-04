77d307fb5a758439675f43fb919a7b207f73da84
Adriano di Lauro <a.dilauro@morganspa.com>
2013-11-04 11:35:34 +0100

    corretion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
       user.email = email
       user.location_id = params[:location][SETTINGS['location_types'].last.downcase] if params[:location].has_key? SETTINGS['location_types'].last.downcase
       user.location_id = nil if user.location_id.to_i == 0
-      user.purchase_id = (@trial && purchase) ? purchase.id : nil
+      user.purchase_id = (!@trial && purchase) ? purchase.id : nil
     end
     if @user.save
       UserMailer.account_confirmation(@user, request.host, request.port).deliver
