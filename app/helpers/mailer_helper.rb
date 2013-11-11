00a4559e5f6e26be95c6da31ac3f9a8d0b514b63
Maurizio De Santis <m.desantis@morganspa.com>
2013-11-11 17:09:19 +0100

    Mailer: aggiunta opzione protocol alla creazione degli url

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -1,12 +1,13 @@
 # Module containing helpers for mailer
 module MailerHelper
-  
-  # Adds the host and a port to values to be passed to an +url_for+ method
-  def host_and_port(*url_for_args)
+
+  def mailer_url_for(*url_for_args)
     return url_for_args unless @host
-    host_port = { host: @host }
+
+    host_port = { protocol: @protocol, host: @host }
     host_port[:port] = @port if @port != 80
     last = url_for_args.last
+
     if last.instance_of? Hash
       url_for_args[0, url_for_args.size - 1] + [last.merge(host_port)]
     else
