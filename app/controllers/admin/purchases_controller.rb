1f884cfac752eb7ee45dcfa810884416e6add7ee
Adriano di Lauro <a.dilauro@morganspa.com>
2013-11-13 15:52:14 +0100

    notifica rinnovo account

--- app/controllers/admin/purchases_controller.rb
+++ app/controllers/admin/purchases_controller.rb
@@ -135,6 +135,7 @@ class Admin::PurchasesController < AdminController
         renewed.users.each do |u|
           u.purchase_id = @purchase.id
           u.save
+          Notification.send_to u.id, t('notifications.account.renewed', :expiration_date => TimeConvert.to_string(@purchase.expiration_date))
         end
         renewed.expiration_date = Time.zone.now
         renewed.save
