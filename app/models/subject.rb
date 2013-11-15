6b18b23cbc0f20eff1b3ea46c2c43337335e178e
Adriano di Lauro <a.dilauro@morganspa.com>
2013-11-15 15:12:12 +0100

    subjects, sempre loro

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -64,10 +64,11 @@ class Subject < ActiveRecord::Base
   # Used to assign a cathegory to each subject
   #
   def self.extract_with_cathegories
+    cathegories = SETTINGS['subject_cathegories']
+    return [{:label => nil, :items => Subject.order(:description)}] if cathegories.blank?
     resp = []
     taken_subjects = []
-    cathegories = SETTINGS['subject_cathegories']
-    (cathegories.present? ? cathegories : []).each do |cat|
+    cathegories.each do |cat|
       resp << {:label => cat[0], :items => Subject.where(:id => cat[1]).order(:description)}
       taken_subjects += cat[1]
     end
