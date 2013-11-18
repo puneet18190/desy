e4a83bd840533d4765c34f6a89c04abaddb16b49
Adriano di Lauro <a.dilauro@morganspa.com>
2013-11-18 12:56:02 +0100

    initiate le FAQQQQSSSS

---
+++ app/controllers/faqs_controller.rb
@@ -0,0 +1,29 @@
+# == Description
+#
+# Contains the FAQS, organized into cathegories
+#
+class FaqsController < ApplicationController
+  
+  layout 'faqs'
+  
+  # Index of all the FAQS
+  def index
+  end
+  
+  # A FAQ about lessons
+  def lessons
+  end
+  
+  # A FAQ about media elements
+  def media_elements
+  end
+  
+  # A FAQ about the Virtual Classroom
+  def virtual_classroom
+  end
+  
+  # A FAQ about user's profile
+  def profile
+  end
+  
+end
