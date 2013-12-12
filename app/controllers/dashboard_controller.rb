5c5eac3598698b9e7ce6144ff112c3525e44ab25
Adriano di Lauro <a.dilauro@morganspa.com>
2013-12-12 11:42:23 +0100

    avanti dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -25,9 +25,9 @@ class DashboardController < ApplicationController
   # * ApplicationController#initialize_layout
   #
   def index
-    @lessons = current_user.suggested_lessons(@lessons_for_page * @lesson_pages)
+    @lessons = current_user.suggested_lessons(@lessons_for_page * @lesson_raws)
     @lessons_emptied = Lesson.dashboard_emptied? current_user.id
-    @media_elements = current_user.suggested_media_elements(@media_elements_for_page * @media_element_pages)
+    @media_elements = current_user.suggested_media_elements(@media_elements_for_page * @media_element_raws)
     @media_elements_emptied = MediaElement.dashboard_emptied? current_user.id
     render_js_or_html_index
   end
@@ -37,12 +37,12 @@ class DashboardController < ApplicationController
   # Initializes all the parameters of pagination
   def initialize_pagination
     @lessons_for_page = correct_integer?(params['lessons_for_page']) ? params['lessons_for_page'].to_i : 0
-    @lesson_pages = SETTINGS['lesson_pages_in_dashboard']
+    @lesson_raws = 6
     @media_elements_for_page = correct_integer?(params['media_elements_for_page']) ? params['media_elements_for_page'].to_i : 0
-    @media_element_pages = SETTINGS['media_element_pages_in_dashboard']
+    @media_element_raws = 6
     if @lessons_for_page != 0 && @media_elements_for_page != 0
-      @lesson_pages = 1
-      @media_element_pages = 1
+      @lesson_raws = 1
+      @media_element_raws = 1
     end
     @lessons_for_page = 0 if @lessons_for_page > 50
     @media_elements_for_page = 0 if @media_elements_for_page > 50
