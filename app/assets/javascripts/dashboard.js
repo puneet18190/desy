e1027376e3fa73d212a6250f5266bcb65c0daffd
Adriano di Lauro <a.dilauro@morganspa.com>
2013-12-16 15:20:11 +0100

    AVANTI

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -192,12 +192,12 @@ function dashboardResizeController() {
 
 function emptyAllPagesInDashboard(selector) {
   var container = $('#dashboard_container .space_' + selector);
-  container.find('.page1').html('');
-  container.find('.page2').html('');
-  container.find('.page3').html('');
-  container.find('.page4').html('');
-  container.find('.page5').html('');
-  container.find('.page6').html('');
+  container.find('.page1').html('').show();
+  container.find('.page2').html('').hide();
+  container.find('.page3').html('').hide();
+  container.find('.page4').html('').hide();
+  container.find('.page5').html('').hide();
+  container.find('.page6').html('').hide();
 }
 
 function resizeLessonsAndMediaElementsInDashboard(lessons, media_elements) {
