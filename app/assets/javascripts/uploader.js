453bf40dcd3be1c3a6beed535ce3a5b3846ee29b
Adriano di Lauro <a.dilauro@morganspa.com>
2013-12-20 11:04:28 +0100

    mangio una mela

--- app/assets/javascripts/uploader.js
+++ app/assets/javascripts/uploader.js
@@ -16,9 +16,8 @@ Handles 413 status error, file too large.
 function uploadDone(selector) {
   var ret = document.getElementById('upload_target').contentWindow.document.title;
   if(ret && ret.match(/413/g)) {
-    $('.barraLoading img').hide();
-    $('iframe').before('<p class="too_large" style="padding: 20px 0 0 40px;"><img src="/assets/puntoesclamativo.png" style="margin: 20px 5px 0 20px;"><span class="lower" style="color:black">' + $('#load-media-element').data('media-file-too-large') + '</span></p>');
-    $('#media_element_media_show').text($('#load-media-element').data('placeholder-media'));
+    // TODO metti messaggio $('#load-' + selector).data('media-file-too-large')
+    // TODO metti in rosso il campo del  file
   }
   return false;
 }
@@ -29,9 +28,7 @@ Reloads media elements page after new media element is successfully loaded.
 @for UploadCallbacks
 **/
 function uploadRedirect(selector) {
-  $('.barraLoading').css('background-color', '#41A62A');
-  $('.barraLoading img').hide();
-  $('.barraLoading img').attr('src', '');
+  // TODO colorare tutta la barra e stoppare l'animazione
   window.location = '/media_elements';
 }
 
@@ -60,9 +57,8 @@ function mediaElementLoaderDocumentReady() {
     closePopUp('load-media-element');
   })
   $body.on('click', '#new_media_element_submit', function() {
-    $('input,textarea').removeClass('form_error');
-    $('.barraLoading img').show();
-    $('.barraLoading img').attr('src', '/assets/loadingBar.gif');
+    $('input, textarea').removeClass('form_error');
+    // TODO far partire l'animazione della barra colorata
     $(this).closest('#new_media_element').submit();
   });
   $body.on('focus', '#load-media-element #title', function() {
@@ -108,8 +104,7 @@ function documentsDocumentReadyUploader() {
   })
   $body.on('click', '#new_document_submit', function() {
     $('input,textarea').removeClass('form_error');
-    $('.barraLoading img').show();
-    $('.barraLoading img').attr('src', '/assets/loadingBar-document.gif');
+    // TODO far partire l'animazione della barra colorata
     $(this).closest('#new_document').submit();
   });
   $body.on('focus', '#load-document #title', function() {
@@ -145,19 +140,22 @@ Update form fields with error labels.
 @param errors {Array} errors list
 **/
 function uploaderErrorsDocuments(errors, fields) {
-  for (var i = 0; i < errors.length; i++) {
-    var error = errors[i];
-    if(error == 'attachment') {
-      $('#load-document #document_attachment_show').addClass('form_error');
-    } else {
-      $('#load-document #' + error).addClass('form_error');
-      if($('#load-document #' + error + '_placeholder').val() == '') {
-        $('#load-document #' + error).val('');
-      }
-    }
-  }
-  $('.barraLoading img').hide();
-  $('.barraLoading img').attr('src', '');
+
+console.log(errors);
+console.log(fields);
+
+//  for (var i = 0; i < errors.length; i++) {
+//    var error = errors[i];
+//    if(error == 'attachment') {
+//      $('#load-document #document_attachment_show').addClass('form_error');
+//    } else {
+//      $('#load-document #' + error).addClass('form_error');
+//      if($('#load-document #' + error + '_placeholder').val() == '') {
+//        $('#load-document #' + error).val('');
+//      }
+//    }
+//  }
+//  // TODO interrompere animazione e togliere colore alla barra
 }
 
 /**
@@ -167,20 +165,23 @@ Update form fields with error labels.
 @param errors {Array} errors list
 **/
 function uploaderErrorsMediaElements(errors, fields) {
-  for (var i = 0; i < errors.length; i++) {
-    var error = errors[i];
-    if(error == 'media') {
-      $('#load-media-element #media_element_media_show').addClass('form_error');
-    } else if(error == 'tags') {
-      $('#load-media-element ._tags_container').addClass('form_error');
-      $('#load-media-element ._tags_container ._placeholder').hide();
-    } else {
-      $('#load-media-element #' + error).addClass('form_error');
-      if($('#load-media-element #' + error + '_placeholder').val() == '') {
-        $('#load-media-element #' + error).val('');
-      }
-    }
-  }
-  $('.barraLoading img').hide();
-  $('.barraLoading img').attr('src', '');
+
+console.log(errors);
+console.log(fields);
+
+//  for (var i = 0; i < errors.length; i++) {
+//    var error = errors[i];
+//    if(error == 'media') {
+//      $('#load-media-element #media_element_media_show').addClass('form_error');
+//    } else if(error == 'tags') {
+//      $('#load-media-element ._tags_container').addClass('form_error');
+//      $('#load-media-element ._tags_container ._placeholder').hide();
+//    } else {
+//      $('#load-media-element #' + error).addClass('form_error');
+//      if($('#load-media-element #' + error + '_placeholder').val() == '') {
+//        $('#load-media-element #' + error).val('');
+//      }
+//    }
+//  }
+//  // TODO interrompere animazione e togliere colore alla barra
 }
