400f98c73967c303b97dde823a66de0e8f28d944
Adriano di Lauro <a.dilauro@morganspa.com>
2014-01-08 11:54:35 +0100

    avanti

--- app/assets/javascripts/lesson_editor.js
+++ app/assets/javascripts/lesson_editor.js
@@ -488,11 +488,11 @@ Initializer for window resize.
 @for LessonEditorDocumentReady
 **/
 function lessonEditorDocumentReadyResize() {
-  $html.find('.lesson-editor-layout ul#slides').css('margin-top', ((($window.height() - 590) / 2) - 40) + 'px');
-  $html.find('.lesson-editor-layout ul#slides.new').css('margin-top', ((($window.height() - 590) / 2)) + 'px');
+  $('.lesson-editor-layout ul#slides').css('margin-top', ((($window.height() - 590) / 2) - 40) + 'px');
+  $('.lesson-editor-layout ul#slides.new').css('margin-top', ((($window.height() - 590) / 2)) + 'px');
   $window.resize(function() {
-    $html.find('.lesson-editor-layout ul#slides').css('margin-top', ((($window.height() - 590) / 2) - 40) + 'px');
-    $html.find('.lesson-editor-layout ul#slides.new').css('margin-top', ((($window.height() - 590) / 2)) + 'px');
+    $('.lesson-editor-layout ul#slides').css('margin-top', ((($window.height() - 590) / 2) - 40) + 'px');
+    $('.lesson-editor-layout ul#slides.new').css('margin-top', ((($window.height() - 590) / 2)) + 'px');
     if(WW > 1000) {
       $('ul#slides li:first').css('margin-left', (($window.width() - 900) / 2) + 'px');
       $('ul#slides.new li:first').css('margin-left', (($window.width() - 900) / 2) + 'px');
