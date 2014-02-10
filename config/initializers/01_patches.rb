d4ef1a365b960bbfea90164c98864e6cea715f5b
Adriano di Lauro <a.dilauro@morganspa.com>
2014-02-10 10:32:47 +0100

    tolta patch eccessiva di I18n

--- config/initializers/01_patches.rb
+++ config/initializers/01_patches.rb
@@ -72,26 +72,6 @@ class ActiveRecord::Base
   end
 end
 
-# HTML escaping by default using +I18n.t+
-module I18n
-  include ERB::Util
-  
-  class << self
-    
-    def translate_with_html_escaping(*args)
-      if args[-1].is_a?(Hash)
-        args[-1] = Hash[ args[-1].map{ |k,v| [k, ([:locale, :throw, :raise, :link, :resolve, :default].include?(k) ? v : ERB::Util.h(v)) ] } ]
-      end
-      translate_without_html_escaping(*args)
-    end
-    
-    alias_method_chain :translate, :html_escaping
-    alias_method :t, :translate_with_html_escaping
-    
-  end
-
-end
-
 # Fixes ActiveRecord bug which crashes on certain conditions when translates PostgreSQL exceptions
 module ActiveRecord
   module ConnectionAdapters
