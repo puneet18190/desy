e6667e568069fd9f6c31aa52bdc3d5dea0f0ccd8
Adriano di Lauro <a.dilauro@morganspa.com>
2014-02-21 15:46:43 +0100

    correzione

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -280,7 +280,7 @@ class ApplicationController < ActionController::Base
     max_description = t('language_parameters.media_element.length_description')
     resp[:title] = t('forms.error_captions.title_too_long', :max => max_title).downcase if errors.added? :title, :too_long, {:count => max_title}
     resp[:title] = t('forms.error_captions.title_blank').downcase if errors.added? :title, :blank
-    resp[:description] = t('forms.error_captions.description_too_long', :max => max_title).downcase if errors.added? :description, :too_long, {:count => max_description}
+    resp[:description] = t('forms.error_captions.description_too_long', :max => max_description).downcase if errors.added? :description, :too_long, {:count => max_description}
     resp[:description] = t('forms.error_captions.description_blank').downcase if errors.added? :description, :blank
     resp[:tags] = t('forms.error_captions.tags_are_not_enough').downcase if errors.added? :tags, :are_not_enough
     resp[:tags] = t('forms.error_captions.tags_too_many').downcase if errors.added? :tags, :too_many
