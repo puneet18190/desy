abcc6028322497ce3e5ff26a24a7dd4f8b2939f7
Adriano di Lauro <a.dilauro@morganspa.com>
2014-02-24 18:25:33 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -149,6 +149,7 @@ Desy::Application.routes.draw do
   post 'virtual_classroom/:lesson_id/remove_lesson'  => 'virtual_classroom#remove_lesson'
   
   # LESSON EDITOR
+  get  'lesson_editor/check_conversion'                        => 'lesson_editor#check_conversion'
   post 'lessons/create'                                        => 'lesson_editor#create'
   get  'lessons/:lesson_id/edit'                               => 'lesson_editor#edit',                  :as => :edit_lesson
   get  'lessons/:lesson_id/slides/edit'                        => 'lesson_editor#index',                 :as => :lesson_editor
