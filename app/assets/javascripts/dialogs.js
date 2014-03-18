a60637a4898cfe745b51cc8f59979cd7232fb724
Adriano di Lauro <a.dilauro@morganspa.com>
2014-03-18 17:37:08 +0100

    avanti

--- app/assets/javascripts/dialogs.js
+++ app/assets/javascripts/dialogs.js
@@ -392,8 +392,8 @@ function showLoadDocumentPopUp() {
           obj.find('.docload_description_placeholder').val('');
           $('#document_attachment_show').text(obj.data('placeholder-attachment'));
           obj.find('.form_error').removeClass('form_error');
+          obj.find('.errors_layer').hide();
           $('#new_document_input').val('');
-          obj.find('.barraLoading .loading-errors').html('');
         }, 100);
       }
     });
@@ -431,8 +431,8 @@ function showLoadMediaElementPopUp() {
           obj.find('._tags_container ._placeholder').show();
           $('#media_element_media_show').val(obj.data('placeholder-media'));
           obj.find('.form_error').removeClass('form_error');
+          obj.find('.errors_layer').hide();
           $('#new_media_element_input').val('');
-          obj.find('.barraLoading .loading-errors').html('');
         }, 100);
       }
     });
