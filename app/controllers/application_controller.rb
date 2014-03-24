50aff5165482b0db95ef9f0be7e8f1622bc592a3
Adriano di Lauro <a.dilauro@morganspa.com>
2014-03-24 16:19:01 +0100

    avatni

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -253,8 +253,8 @@ class ApplicationController < ActionController::Base
     resp[:title] = t('forms.error_captions.title_blank').downcase if errors.added? :title, :blank
     resp[:description] = t('forms.error_captions.description_too_long', :max => max_description).downcase if errors.added? :description, :too_long, {:count => max_description}
     resp[:description] = t('forms.error_captions.description_blank').downcase if errors.added? :description, :blank
-    resp[:tags] = t('forms.error_captions.tags_are_not_enough').downcase if errors.added? :tags, :are_not_enough # TODO traduzz
-    resp[:tags] = t('forms.error_captions.tags_too_many').downcase if errors.added? :tags, :too_many # TODO traduzz
+    resp[:tags] = t('forms.error_captions.tags_are_not_enough').downcase if errors.added? :tags, :are_not_enough
+    resp[:tags] = t('forms.error_captions.tags_too_many').downcase if errors.added? :tags, :too_many
     resp[:subject] = t('forms.error_captions.subject_missing_in_lesson') if errors.added? :subject_id, :blank
   end
   
